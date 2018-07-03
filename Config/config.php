99ba213f481d59788ecf23885cece06ff819bd27
jcec007 <jcec007@gmail.com>
2018-07-03 16:26:17 -0500

    Revert "=renamed shipping and payment modules"
    This reverts commit 02300a579812670bb047c4c1914dbf5e7957e35a.

--- Config/config.php
+++ Config/config.php
@@ -14,7 +14,7 @@ return [
     	'freeshipping'=> [
     		'name' => 'freeshipping',
     		'title' => trans('ifreeshipping::configurations.single') ,
-    		'entity' => '\Modules\Icommerce_freeshipping\Entities\Configuration',
+    		'entity' => '\Modules\Ifreeshipping\Entities\Configuration',
     		'view' => 'ifreeshipping::admin.configurations.index'
     	],
         */
@@ -23,13 +23,13 @@ return [
         'flatrate'=> [
             'name' => 'flatrate',
             'title' => trans('iflatrate::configflatrates.single') ,
-            'entity' => '\Modules\Icommerce_flatrate\Entities\Configflatrate',
+            'entity' => '\Modules\Iflatrate\Entities\Configflatrate',
             'view' => 'iflatrate::admin.configflatrates.index',
             'msjini' => trans('iflatrate::configflatrates.messages.msjini'),
             'init' => "iflatrate_Init"
         ],
         */
-
+       
 
         /*
         'localdelivery'=> [
@@ -43,16 +43,16 @@ return [
         'iagree'=> [
             'name' => 'iagree',
             'title' => trans('iagree::configagrees.single') ,
-            'entity' => '\Modules\Icommerce_agree\Entities\Configagree',
+            'entity' => '\Modules\Iagree\Entities\Configagree',
             'view' => 'iagree::admin.configagrees.index',
             'msjini' => trans('iagree::configagrees.messages.msjini'),
             'init' => "iagree_Init"
         ],
-
+        
         'ups'=> [
             'name' => 'ups',
             'title' => trans('iups::configups.single') ,
-            'entity' => '\Modules\Icommerce_ups\Entities\Configups',
+            'entity' => '\Modules\Iups\Entities\Configups',
             'view' => 'iups::admin.configups.index',
             'msjini' => trans('iups::configups.messages.msjini'),
             'init' => "iups_Init"
@@ -61,7 +61,7 @@ return [
         'usps'=> [
             'name' => 'usps',
             'title' => trans('iusps::configusps.single') ,
-            'entity' => '\Modules\Icommerce_usps\Entities\Configusps',
+            'entity' => '\Modules\Iusps\Entities\Configusps',
             'view' => 'iusps::admin.configusps.index',
             'msjini' => trans('iusps::configusps.messages.msjini'),
             'init' => "iusps_Init"
@@ -74,14 +74,14 @@ return [
         'paypal'=> [
             'name' => 'paypal',
             'title' => trans('ipaypal::paypalconfigs.single') ,
-            'entity' => '\Modules\Icommerce_paypal\Entities\Paypalconfig',
+            'entity' => '\Modules\Ipaypal\Entities\Paypalconfig',
             'view' => 'ipaypal::admin.paypalconfigs.index'
         ],
 
         'checkormoney'=> [
             'name' => 'checkormoney',
             'title' => trans('icheckmoney::checkmoneyconfigs.single') ,
-            'entity' => '\Modules\Icommerce_checkmoney\Entities\Checkmoneyconfig',
+            'entity' => '\Modules\Icheckmoney\Entities\Checkmoneyconfig',
             'view' => 'icheckmoney::admin.checkmoneyconfigs.index'
         ],
 
