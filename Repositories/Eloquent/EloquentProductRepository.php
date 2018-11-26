17d7717a1a1b29fbd17e29d17b3bba9fd5a05bc8
Sabas Vega <sabascarloseduardo@gmail.com>
2018-11-26 10:35:07 -0400

    Filter product options adjusted to receive several options.

--- Repositories/Eloquent/EloquentProductRepository.php
+++ Repositories/Eloquent/EloquentProductRepository.php
@@ -361,10 +361,11 @@ class EloquentProductRepository extends EloquentBaseRepository implements Produc
                         '=', 'icommerce__products.id')
                         ->whereIn('icommerce__product_category.category_id', $filter->categories);
                 }
-                if (isset($filter->option)) {
-                  $query->leftJoin('icommerce__product_option', 'icommerce__product_option.product_id',
-                  '=', 'icommerce__products.id')
-                  ->where('icommerce__product_option.option_id', $filter->option);
+                if (isset($filter->options)) {
+                  is_array($filter->options) ? true : $filter->options = [$filter->options];
+                    $query->leftJoin('icommerce__product_option', 'icommerce__product_option.product_id',
+                        '=', 'icommerce__products.id')
+                        ->whereIn('icommerce__product_option.option_id', $filter->options);
                 }
                 if (isset($filter->options_values)) {
                     is_array($filter->options_values) ? true : $filter->options_values = [$filter->options_values];
