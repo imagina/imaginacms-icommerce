c753a1face23a33b7e928f284ef053a1fc573139
Yeison Tapia <yftapial@ut.edu.co>
2018-12-29 13:00:33 -0500

    fix upddateby with fields translatables

--- Repositories/Eloquent/EloquentCategoryRepository.php
+++ Repositories/Eloquent/EloquentCategoryRepository.php
@@ -7,27 +7,27 @@ use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 
 class EloquentCategoryRepository extends EloquentBaseRepository implements CategoryRepository
 {
-  
+
   public function index($params)
   {
-    
+
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // RELATIONSHIPS
     $defaultInclude = ['translations'];
     $query->with(array_merge($defaultInclude, $params->include));
-    
+
     // FILTERS
     if ($params->filter) {
       $filter = $params->filter;
-      
+
       //get language translation
       $lang = \App::getLocale();
-      
+
       //add filter by search
       if (isset($filter->search)) {
-        
+
         //find search in columns
         $query->where(function ($query) use ($filter, $lang) {
           $query->whereHas('translations', function ($query) use ($filter, $lang) {
@@ -39,47 +39,47 @@ class EloquentCategoryRepository extends EloquentBaseRepository implements Categ
         });
       }
     }
-    
+
     // FIELDS
     if ($params->fields) {
       $query->select($params->fields);
     }
-    
+
     // PAGE & TAKE
     //Return request with pagination
     if ($params->page) {
       $params->take ? true : $params->take = 12; //If no specific take, query take 12 for default
       return $query->paginate($params->take);
     }
-    
+
     //Return request without pagination
     if (!$params->page) {
       $params->take ? $query->take($params->take) : false; //if request to take a limit
       return $query->get();
     }
   }
-  
+
   public function show($criteria, $params)
   {
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // RELATIONSHIPS
     $includeDefault = ['translations'];
     $query->with(array_merge($includeDefault, $params->include));
-  
+
     /*== FIELDS ==*/
     if (is_array($params->fields) && count($params->fields))
       $query->select($params->fields);
-    
+
     // FILTERS
     //get language translation
     $lang = \App::getLocale();
-  
+
     /*== FILTER ==*/
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->slug) && $filter->slug)//Filter by slug
         $result = $query->whereHas('translations', function ($query) use ($criteria, $lang) {
           $query->where('locale', $lang)
@@ -87,56 +87,62 @@ class EloquentCategoryRepository extends EloquentBaseRepository implements Categ
         });
       else//Filter by ID
         $query->where('id', $criteria);
-      
+
     }
     return $query->first();
   }
-  
+
   public function create($data){
-    
+
     $category = $this->model->create($data);
-    
+
     return $category;
   }
-  
-  
+
+
   public function updateBy($criteria, $data, $params){
-    
+
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // FILTER
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->field))//Where field
         $query->where($filter->field, $criteria);
       else//where id
         $query->where('id', $criteria);
     }
-    
+
     // REQUEST
-    $query->update($data);
+    $model = $query->first();
+
+    if($model) {
+      $model->update($data);
+    }
+    return $model;
+
   }
-  
+
   public function deleteBy($criteria, $params)
   {
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // FILTER
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->field)) //Where field
         $query->where($filter->field, $criteria);
       else //where id
         $query->where('id', $criteria);
     }
-    
+
     /*== REQUEST ==*/
     $query->delete();
   }
-  
-  
+
+
 }
