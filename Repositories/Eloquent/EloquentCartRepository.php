08b73ea5b78491079dd06bd7104c93c00a462f0a
Yeison Tapia <yftapial@ut.edu.co>
2019-02-11 16:40:22 -0500

    + Filter response for user_id and Ip

--- Repositories/Eloquent/EloquentCartRepository.php
+++ Repositories/Eloquent/EloquentCartRepository.php
@@ -11,15 +11,15 @@ class EloquentCartRepository extends EloquentBaseRepository implements CartRepos
   {
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // RELATIONSHIPS
     $defaultInclude = ['products'];
     $query->with(array_merge($defaultInclude,$params->include));
-    
+
     // FILTERS
     if($params->filter) {
       $filter = $params->filter;
-      
+
       //add filter by search
       if (isset($filter->search)) {
         //find search in columns
@@ -27,12 +27,20 @@ class EloquentCartRepository extends EloquentBaseRepository implements CartRepos
           ->orWhere('updated_at', 'like', '%' . $filter->search . '%')
           ->orWhere('created_at', 'like', '%' . $filter->search . '%');
       }
-      
+
+      if (isset($filter->ip)){
+          $query->where('ip', $filter->ip);
+      }
+
+      if (isset($filter->user)){
+          $query->where('user_id', $filter->user);
+      }
+
     }
     /*== FIELDS ==*/
     if (isset($params->fields) && count($params->fields))
       $query->select($params->fields);
-  
+
     /*== REQUEST ==*/
     if (isset($params->page) && $params->page) {
       return $query->paginate($params->take);
@@ -41,89 +49,89 @@ class EloquentCartRepository extends EloquentBaseRepository implements CartRepos
       return $query->get();
     }
   }
-  
-  
+
+
   public function getItem($criteria, $params)
   {
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     $query->where('id', $criteria);
-    
+
     // RELATIONSHIPS
     $includeDefault = ['products'];
     $query->with(array_merge($includeDefault, $params->include));
-    
+
     // FIELDS
     if ($params->fields) {
       $query->select($params->fields);
     }
     return $query->first();
-    
+
   }
-  
+
   public function create($data){
-    
+
     $cart = $this->model->create($data);
-  
+
     //sync tables
     $cart->products()->sync(array_get($data, 'products', []));
-    
+
     return $cart;
   }
-  
+
   public function updateBy($criteria, $data, $params){
-    
+
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // FILTER
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->field))//Where field
         $query->where($filter->field, $criteria);
       else//where id
         $query->where('id', $criteria);
     }
-    
+
     // REQUEST
     $model = $query->first();
-  
+
     if($model){
       $model->update($data);
-  
+
       //sync tables
       $model->products()->sync(array_get($data, 'products', []));
     }
-    
+
     return $model;
-  
+
   }
-  
+
   public function deleteBy($criteria, $params)
   {
     // INITIALIZE QUERY
     $query = $this->model->query();
-    
+
     // FILTER
     if (isset($params->filter)) {
       $filter = $params->filter;
-      
+
       if (isset($filter->field)) //Where field
         $query->where($filter->field, $criteria);
       else //where id
         $query->where('id', $criteria);
     }
-  
-  
+
+
     // REQUEST
     $model = $query->first();
-  
+
     if($model) {
       $model->delete();
     }
   }
-  
-  
+
+
 }
