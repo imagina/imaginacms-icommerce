5456976a3f1c7ad9dc8874fe821cc6cab41fabd3
Marcos Rativa <marcosr@imaginacolombia.com>
2019-05-27 18:12:04 -0500

    Preparing Version 1.0.7

--- Http/ApiRoutes/categoryRoutes.php
+++ Http/ApiRoutes/categoryRoutes.php
@@ -3,30 +3,29 @@
 use Illuminate\Routing\Router;
 
 $router->group(['prefix' => '/categories'], function (Router $router) {
-  $locale = \LaravelLocalization::setLocale() ?: \App::getLocale();
-  
-  $router->post('/', [
-    'as' => $locale . 'api.icommerce.categories.create',
-    'uses' => 'CategoryApiController@create',
-    'middleware' => ['auth:api']
-  ]);
-  $router->get('/', [
-    'as' => $locale . 'api.icommerce.categories.index',
-    'uses' => 'CategoryApiController@index',
-  ]);
-  $router->put('/{criteria}', [
-    'as' => $locale . 'api.icommerce.categories.update',
-    'uses' => 'CategoryApiController@update',
-    'middleware' => ['auth:api']
-  ]);
-  $router->delete('/{criteria}', [
-    'as' => $locale . 'api.icommerce.categories.delete',
-    'uses' => 'CategoryApiController@delete',
-    'middleware' => ['auth:api']
-  ]);
-  $router->get('/{criteria}', [
-    'as' => $locale . 'api.icommerce.categories.show',
-    'uses' => 'CategoryApiController@show',
-  ]);
-  
+
+    $router->post('/', [
+        'as' => 'api.icommerce.categories.create',
+        'uses' => 'CategoryApiController@create',
+        'middleware' => ['auth:api']
+    ]);
+    $router->get('/', [
+        'as' => 'api.icommerce.categories.index',
+        'uses' => 'CategoryApiController@index',
+    ]);
+    $router->get('/{criteria}', [
+        'as' => 'api.icommerce.categories.show',
+        'uses' => 'CategoryApiController@show',
+    ]);
+    $router->put('/{criteria}', [
+        'as' => 'api.icommerce.categories.update',
+        'uses' => 'CategoryApiController@update',
+        'middleware' => ['auth:api']
+    ]);
+    $router->delete('/{criteria}', [
+        'as' => 'api.icommerce.categories.delete',
+        'uses' => 'CategoryApiController@delete',
+        'middleware' => ['auth:api']
+    ]);
+
 });
--- Http/ApiRoutes/productRoutes.php
+++ Http/ApiRoutes/productRoutes.php
@@ -2,31 +2,28 @@
 
 use Illuminate\Routing\Router;
 
-$router->group(['prefix' => '/products','middleware' => ['auth:api']], function (Router $router) {
-  $locale = \LaravelLocalization::setLocale() ?: \App::getLocale();
-  
-  $router->post('/', [
-    'as' => $locale . 'api.icommerce.products.create',
-    'uses' => 'ProductApiController@create',
-  ]);
-  $router->put('/{criteria}', [
-    'as' => $locale . 'api.icommerce.products.update',
-    'uses' => 'ProductApiController@update',
-  ]);
-  $router->delete('/{criteria}', [
-    'as' => $locale . 'api.icommerce.products.delete',
-    'uses' => 'ProductApiController@delete',
-  ]);
-});
-
 $router->group(['prefix' => '/products'], function (Router $router) {
-    $locale = \LaravelLocalization::setLocale() ?: \App::getLocale();
+    $router->post('/', [
+        'as' => 'api.icommerce.products.create',
+        'uses' => 'ProductApiController@create',
+        'middleware' => ['auth:api']
+    ]);
+    $router->put('/{criteria}', [
+        'as' => 'api.icommerce.products.update',
+        'uses' => 'ProductApiController@update',
+        'middleware' => ['auth:api']
+    ]);
+    $router->delete('/{criteria}', [
+        'as' =>'api.icommerce.products.delete',
+        'uses' => 'ProductApiController@delete',
+        'middleware' => ['auth:api']
+    ]);
     $router->get('/', [
-        'as' => $locale . 'api.icommerce.products.index',
+        'as' =>'api.icommerce.products.index',
         'uses' => 'ProductApiController@index',
     ]);
     $router->get('/{criteria}', [
-        'as' => $locale . 'api.icommerce.products.show',
+        'as' => 'api.icommerce.products.show',
         'uses' => 'ProductApiController@show',
     ]);
 
