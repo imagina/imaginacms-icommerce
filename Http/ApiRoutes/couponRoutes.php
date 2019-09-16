e3f4cd6a75fab3510af935a126d2c4d3f9d99611
Yeison Tapia <yftapial@ut.edu.co>
2019-09-16 14:44:31 -0500

    + Add coupon Routes

--- Http/ApiRoutes/couponRoutes.php
+++ Http/ApiRoutes/couponRoutes.php
@@ -4,11 +4,16 @@ use Illuminate\Routing\Router;
 
 $router->group(['prefix' => '/coupons'/*,'middleware' => ['auth:api']*/], function (Router $router) {
   $locale = \LaravelLocalization::setLocale() ?: \App::getLocale();
-  
-  
+
+  $router->get('/coupons-validate', [
+    'as' => $locale . 'api.icommerce.coupons.validate',
+    'uses' => 'CouponApiController@validateCoupon',
+    'middleware' => ['auth:api']
+  ]);
   $router->post('/', [
     'as' => $locale . 'api.icommerce.coupons.create',
     'uses' => 'CouponApiController@create',
+    'middleware' => ['auth:api']
   ]);
   $router->get('/', [
     'as' => $locale . 'api.icommerce.coupons.index',
@@ -17,14 +22,17 @@ $router->group(['prefix' => '/coupons'/*,'middleware' => ['auth:api']*/], functi
   $router->put('/{criteria}', [
     'as' => $locale . 'api.icommerce.coupons.update',
     'uses' => 'CouponApiController@update',
+    'middleware' => ['auth:api']
   ]);
   $router->delete('/{criteria}', [
     'as' => $locale . 'api.icommerce.coupons.delete',
     'uses' => 'CouponApiController@delete',
+    'middleware' => ['auth:api']
   ]);
   $router->get('/{criteria}', [
     'as' => $locale . 'api.icommerce.coupons.show',
     'uses' => 'CouponApiController@show',
   ]);
-  
+
+
 });
