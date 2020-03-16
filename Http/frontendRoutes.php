2a0b226b014fea6642c73134c19301171b56f313
Marcos Rativa <marcosr@imaginacolombia.com>
2020-03-16 09:19:31 -0500

    Preparing Version 1.1.2

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -3,26 +3,27 @@
 use Illuminate\Routing\Router;
 
 
-$categoryRepository = app('Modules\Icommerce\Repositories\CategoryRepository');
-$categories = $categoryRepository->getItemsBy(json_decode(json_encode(['fields' => 'id', 'include' => [], 'take' => null])));
 $locale = LaravelLocalization::setLocale() ?: App::getLocale();
 
-foreach ($categories as $category) {
-    /** @var Router $router */
-    $router->group(['prefix' => $category->slug], function (Router $router) use ($locale, $category) {
+if (!App::runningInConsole()) {
+    $categoryRepository = app('Modules\Icommerce\Repositories\CategoryRepository');
+    $categories = $categoryRepository->getItemsBy(json_decode(json_encode(['fields' => 'id', 'include' => [], 'take' => null])));
+    foreach ($categories as $category) {
+        /** @var Router $router */
+        $router->group(['prefix' => $category->slug], function (Router $router) use ($locale, $category) {
 
-        $router->get('/', [
-            'as' => $locale . '.icommerce.category.' . $category->slug,
-            'uses' => 'PublicController@index',
-        ]);
+            $router->get('/', [
+                'as' => $locale . '.icommerce.category.' . $category->slug,
+                'uses' => 'PublicController@index',
+            ]);
 
-        $router->get('{slug}', [
-            'as' => $locale . '.icommerce.' . $category->slug . '.product',
-            'uses' => 'PublicController@show',
-        ]);
-    });
+            $router->get('{slug}', [
+                'as' => $locale . '.icommerce.' . $category->slug . '.product',
+                'uses' => 'PublicController@show',
+            ]);
+        });
+    }
 }
-
 /** @var Router $router */
 $router->group(['prefix' => '/checkout'], function (Router $router) use ($locale) {
 
