3ddcfb7a3f8a4f56a19e44fa0327789d40007737
YeisonTapia <yftapial@ut.edu.co>
2020-04-02 16:05:48 -0500

    Add support class for update order in values option.

--- Support/OptionValuesOrdener.php
+++ Support/OptionValuesOrdener.php
@@ -10,4 +10,109 @@ class OptionValuesOrdener
 {
 
 
+  /**
+   * @var OptionValueRepository
+   */
+  private $optionValueRepository;
+
+  /**
+   * @param OptionValueRepository $optionValueRepository
+   */
+  public function __construct(OptionValueRepository $optionValueRepository)
+  {
+    $this->optionValueRepository = $optionValueRepository;
+  }
+
+  /**
+   * @param $data
+   */
+  public function handle($data)
+  {
+    $data = $this->convertToArray(json_decode($data));
+
+    foreach ($data as $position => $item) {
+      $this->order($position, $item);
+    }
+  }
+
+  /**
+   * Order recursively the menu items
+   * @param int   $position
+   * @param array $item
+   */
+  private function order($position, $item)
+  {
+    $optionValue = $this->optionValueRepository->find($item['id']);
+    if (0 === $position && false === $optionValue->isRoot()) {
+      return;
+    }
+    $this->savePosition($optionValue, $position);
+    $this->makeItemChildOf($optionValue, null);
+
+    if ($this->hasChildren($item)) {
+      $this->handleChildrenForParent($optionValue, $item['children']);
+    }
+  }
+
+  /**
+   * @param Menuitem $parent
+   * @param array    $children
+   */
+  private function handleChildrenForParent(OptionValue $parent, array $children)
+  {
+    foreach ($children as $position => $item) {
+      $optionValue = $this->optionValueRepository->find($item['id']);
+      $this->savePosition($optionValue, $position);
+      $this->makeItemChildOf($optionValue, $parent->id);
+
+      if ($this->hasChildren($item)) {
+        $this->handleChildrenForParent($optionValue, $item['children']);
+      }
+    }
+  }
+
+  /**
+   * Save the given position on the menu item
+   * @param object $optionValue
+   * @param int    $position
+   */
+  private function savePosition($optionValue, $position)
+  {
+    $this->optionValueRepository->update($optionValue, compact('position'));
+  }
+
+  /**
+   * Check if the item has children
+   *
+   * @param  array $item
+   * @return bool
+   */
+  private function hasChildren($item)
+  {
+    return isset($item['children']);
+  }
+
+  /**
+   * Set the given parent id on the given menu item
+   *
+   * @param object $item
+   * @param int    $parent_id
+   */
+  private function makeItemChildOf($item, $parent_id)
+  {
+    $this->optionValueRepository->update($item, compact('parent_id'));
+  }
+
+  /**
+   * Convert the object to array
+   * @param $data
+   * @return array
+   */
+  private function convertToArray($data)
+  {
+    $data = json_decode(json_encode($data), true);
+
+    return $data;
+  }
+
 }
