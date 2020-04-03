9e9f1247acfb2a240db8f20209f7b7b90c80f3f9
YeisonTapia <yftapial@ut.edu.co>
2020-04-03 15:36:03 -0500

    Clean support file for ShippingMethod

--- Support/ShippingMethod.php
+++ Support/ShippingMethod.php
@@ -7,33 +7,9 @@ class ShippingMethod
 
     public function fixDataSend($data){
 
-        $products = [];
-
-        /*
-        foreach($cart->products as $product){
-            array_push($products, [
-            "title" => $product->product->title,
-            "price" => $product->price,
-            "weight" => $product->product->weight,
-            "length" => $product->product->lenght,
-            "width" => $product->product->width,
-            "height" => $product->product->height,
-            "freeshipping" => $product->product->freeshipping,
-            "quantity" => $product->quantity
-            ]);
-        }
-
-        $dataMethods['products'] = array(
-            "items" => json_encode($products),
-            "total" => $cart->getTotalAttribute()
-        );
-        */
-
         $dataMethods['products'] = array(
           "cart_id" => $data->cart->id
         );
-        
-        
 
         if(isset($data->addressShipping->country)){
           $data['countryCode'] = $data->addressShipping->country->iso_2;
@@ -52,12 +28,6 @@ class ShippingMethod
           $data['zone'] = $data->addressShipping->province->translate('en')->name;
           $data['zoneCode'] = $data->addressShipping->province->iso_2; //Optional
         }
-        
-        if(!empty($data->areaMapId))
-          $data['areaMapId'] = $areaMapId;
-
-        if(!empty($data->shippingValue))
-          $data['shippingValue'] = $shippingValue;
 
         $dataMethods['options'] = $data;
 
@@ -66,9 +36,8 @@ class ShippingMethod
     }
 
     public function searchPriceByName($shippingMethods,$dataName){
-        
-        foreach ($shippingMethods as $shipping) {
 
+        foreach ($shippingMethods as $shipping) {
             if($shipping->calculations->status=="success"){
               if($shipping->calculations->items==null){
                 if($shipping->name==$dataName){
