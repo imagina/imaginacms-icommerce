b2d1ba1a5cb5dc185a6231b4501f6207a7567750
YeisonTapia <yftapial@ut.edu.co>
2020-04-06 16:50:53 -0500

    Update wishlist routes, added auth:api middleware

--- Http/ApiRoutes/wishlistRoutes.php
+++ Http/ApiRoutes/wishlistRoutes.php
@@ -2,28 +2,33 @@
 
 use Illuminate\Routing\Router;
 
-$router->group(['prefix' => '/wishlists'/*,'middleware' => ['auth:api']*/], function (Router $router) {
+$router->group(['prefix' => '/wishlists'], function (Router $router) {
   $locale = \LaravelLocalization::setLocale() ?: \App::getLocale();
   
   $router->post('/', [
     'as' => $locale . 'api.icommerce.wishlists.create',
     'uses' => 'WishlistApiController@create',
+    'middleware' => ['auth:api']
   ]);
   $router->get('/', [
     'as' => $locale . 'api.icommerce.wishlists.index',
     'uses' => 'WishlistApiController@index',
+    'middleware' => ['auth:api']
   ]);
   $router->put('/{criteria}', [
     'as' => $locale . 'api.icommerce.wishlists.update',
     'uses' => 'WishlistApiController@update',
+    'middleware' => ['auth:api']
   ]);
   $router->delete('/{criteria}', [
     'as' => $locale . 'api.icommerce.wishlists.delete',
     'uses' => 'WishlistApiController@delete',
+    'middleware' => ['auth:api']
   ]);
   $router->get('/{criteria}', [
     'as' => $locale . 'api.icommerce.wishlists.show',
     'uses' => 'WishlistApiController@show',
+    'middleware' => ['auth:api']
   ]);
   
 });
