ce2a1ae5d1e521dd86a13aae60f06c9d80cd8b84
YeisonTapia <yftapial@ut.edu.co>
2020-04-08 10:50:16 -0500

    Update TaxClassApiController with new standar.

--- Http/Controllers/Api/TaxClassApiController.php
+++ Http/Controllers/Api/TaxClassApiController.php
@@ -85,9 +85,12 @@ class TaxClassApiController extends BaseApiController
   public function create(Request $request)
   {
     try {
-      $taxClass = $this->taxClass->create($request->all());
 
-      $response = ['data' => ''];
+      $data = $request->input('attributes') ?? [];//Get data
+
+      $taxClass = $this->taxClass->create($data);
+
+      $response = ['data' => $taxClass];
 
     } catch (\Exception $e) {
       $status = 500;
@@ -105,19 +108,32 @@ class TaxClassApiController extends BaseApiController
    */
   public function update($criteria, Request $request)
   {
+    \DB::beginTransaction(); //DB Transaction
     try {
+      //Get data
+      $data = $request->input('attributes') ?? [];//Get data
 
-      $taxClass = $this->taxClass->updateBy($criteria, $request->all(), $this->getParamsRequest($request));
+      //Get Parameters from URL.
+      $params = $this->getParamsRequest($request);
 
-      $response = ['data' => ''];
+      $dataEntity = $this->taxClass->getItem($criteria, $params);
 
+      if (!$dataEntity) throw new Exception('Item not found', 204);
+
+      //Request to Repository
+      $this->taxClass->update($dataEntity, $data);
+      //Response
+      $response = ["data" => 'Item Updated'];
+      \DB::commit();//Commit to DataBase
     } catch (\Exception $e) {
-      $status = 500;
-      $response = [
-        'errors' => $e->getMessage()
-      ];
+      \Log::error($e);
+      \DB::rollback();//Rollback to Data Base
+      $status = $this->getStatusError($e->getCode());
+      $response = ["errors" => $e->getMessage()];
     }
-    return response()->json($response, $status ?? 200);
+
+    //Return response
+    return response()->json($response ?? ["data" => "Request successful"], $status ?? 200);
   }
 
   /**
