5adfac2b5e37eaae9318e7a8430b259b0fc28cf1
YeisonTapia <yftapial@ut.edu.co>
2020-04-30 15:29:41 -0500

    Add field geozone_id in shipping methods and payment methods.

---
+++ Database/Migrations/2020_04_30_151216_add_geozone_id_in_shipping_methods.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGeozoneIdInShippingMethods extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('icommerce__shipping_methods', function (Blueprint $table) {
+          $table->integer('geozone_id')->unsigned()->nullable();
+          $table->foreign('geozone_id')->references('id')->on('ilocations__geozones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('icommerce__shipping_methods', function (Blueprint $table) {
+          $table->dropForeign('icommerce__shipping_methods_geozone_id_foreign');
+          $table->dropColumn('geozone_id');
+        });
+    }
+}
