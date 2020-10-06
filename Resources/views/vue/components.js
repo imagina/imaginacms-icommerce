854d7b9f18d68cd9cb68999b210d5f8c1675485f
JCEC007 <jcec007@gmail.com>
2020-10-06 15:58:00 -0500

    +add: new vue components

--- Resources/views/vue/components.js
+++ Resources/views/vue/components.js
@@ -1,1 +1,2 @@
-Vue.component('selectProductOptions', require('./components/icommerce/selectProductOptions.vue'));
+Vue.component('selectProductOptions', require('./components/icommerce/productOptions/selectProductOptions.vue'));
+Vue.component('selectProductOptions', require('./components/icommerce/product/featured.vue'));
