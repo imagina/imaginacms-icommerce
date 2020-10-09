ac8d15cf7b119e85ceae0c5251ca6e44511122f2
msolanogithub <msolano2505@gmail.com>
2020-10-08 19:15:21 -0500

    +Add: Config | "Setting fields"

---
+++ Config/settings-fields.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  'form-emails' => [
+    'name' => 'icommerce::form-emails',
+    'value' => null,
+    'type' => 'input',
+    'props' => [
+      'label' => 'icommerce::common.settings.emails'
+    ],
+  ],
+  'product-per-page' => [
+    'name' => 'icommerce::product-per-page',
+    'value' => null,
+    'type' => 'input',
+    'props' => [
+      'label' => 'icommerce::common.settings.product-per-page'
+    ],
+  ],
+];
