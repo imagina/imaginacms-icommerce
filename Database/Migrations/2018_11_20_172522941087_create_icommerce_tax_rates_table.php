af3366bcdd1ea98b6273a54a29d5cb7df624f1fc
JCEC007 <jcec007@gmail.com>
2020-10-13 15:07:36 -0500

    +fix: fail foreign key removed

--- Database/Migrations/2018_11_20_172522941087_create_icommerce_tax_rates_table.php
+++ Database/Migrations/2018_11_20_172522941087_create_icommerce_tax_rates_table.php
@@ -23,7 +23,6 @@ class CreateIcommerceTaxRatesTable extends Migration
       $table->foreign('geozone_id')->references('id')->on('ilocations__geozones');
       $table->boolean('customer')->default(0);
       $table->integer('tax_class_id')->unsigned();
-      $table->foreign('tax_class_id')->references('id')->on('icommerce__tax_classes');
       $table->timestamps();
     });
   }
