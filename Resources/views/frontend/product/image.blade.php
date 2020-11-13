d3dcc5d88d9a8fb6efcf779250e1f37186c5afbe
William Verde <williamverde10@gmail.com>
2020-11-13 11:12:00 -0400

    Reupload: Added: lazy loading on component image

--- Resources/views/frontend/product/image.blade.php
+++ Resources/views/frontend/product/image.blade.php
@@ -1,12 +1,18 @@
-<div class="bg-img d-flex justify-content-center align-items-center">
-	<a href="{{$product->url}}" title="{{$product->name}}">
-	
-			<figure>
-				<picture>
-					<source type="image/jpeg" srcset="{{$product->mediaFiles()->mainimage->relativeMediumThumb}}">
-						<img src="{{$product->mediaFiles()->mainimage->relativeMediumThumb}}" alt="{{$product->name}}">
-			    </picture>
-		    </figure>
-		
+<div class="bg-img d-flex justify-content-center align-items-center overflow-hidden">
+	<a href="{{$product->new_url}}" title="{{$product->name}}">
+
+		<figure>
+			<picture>
+				<source
+				  	type="image/jpeg"
+				    data-srcset="{{$product->mediaFiles()->mainimage->relativeMediumThumb}}"
+				 />
+				<img 
+					data-src="{{$product->mediaFiles()->mainimage->relativeMediumThumb}}"
+					alt = "{{$product->name}}" 
+					class="lazyload" />
+			</picture>
+		</figure>
+
 	</a>
 </div>
