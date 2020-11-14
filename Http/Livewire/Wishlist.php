b99bf372b158911594de4df17881be86da2b4aac
William Verde <williamverde10@gmail.com>
2020-11-14 13:38:24 -0400

    Updates: Add wishlist

--- Http/Livewire/Wishlist.php
+++ Http/Livewire/Wishlist.php
@@ -15,6 +15,69 @@ class Wishlist extends Component
    
   }
   
+  /**
+   * Get wishlist from user
+   */
+  public function getWishListUser($userId){
+
+    $params = json_decode(json_encode([
+      "filter" => [
+        'user' => $userId
+        ]
+    ]));
+
+    $products = $this->wishlistRepository()->getItemsBy($params);
+
+    return $products;
+  }
+
+  /**
+   * @return cartRepository
+   */
+  public function wishlistRepository()
+  {
+    return  app('Modules\Icommerce\Repositories\WishlistRepository');
+  }
+
+  /**
+  *	Add product to wishlist
+  */
+  public function addWishList($productId){
+
+
+    if(\Auth::user()){
+
+      $userId = \Auth::user()->id;
+      $products = $this->getWishListUser($userId);
+
+      //Falta- Check if the product id has been added
+      if(count($products)>0){
+
+      }
+
+      $data = [
+        "user_id" => $userId;
+        "product_id" => $productId
+      ];
+
+      $this->wishlistRepository()->create($data);
+
+      $this->alert('success', trans('icommerce::wishlist.message.add'),[
+        'position' => 'bottom-end',
+        'iconColor' => setting("isite::brandPrimary","#fff")
+      ]);
+
+
+    }else{
+
+      $this->alert('warning', trans('icommerce::wishlist.message.user login'),[
+        'position' => 'bottom-end',
+        'iconColor' => setting("isite::brandPrimary","#fff")
+      ]);
+
+    }
+
+  }
  
   
   public function render()
