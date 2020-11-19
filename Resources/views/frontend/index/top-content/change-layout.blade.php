1f23291707302badc95e294ab17cc43662a3586f
William Verde <williamverde10@gmail.com>
2020-11-19 09:46:18 -0400

    Added: validation status change layout

--- Resources/views/frontend/index/top-content/change-layout.blade.php
+++ Resources/views/frontend/index/top-content/change-layout.blade.php
@@ -3,9 +3,11 @@
 		{{trans('icommerce::frontend.index.views')}}:
 	</label>
 	<div class="types-columns ml-1">
-		@foreach($this->configs['mainLayout']['options'] as  $layoutOption)
-			<i wire:click="changeLayout('{{$layoutOption['name']}}')" class="{{$layoutOption['icon']}} mx-1 cursor-pointer {{$mainLayout==$layoutOption['name'] ? 'active text-primary' : $layoutOption['name']}}" aria-hidden="true"></i>
-		@endforeach
+		@if($layoutOption['status'])
+			@foreach($this->configs['mainLayout']['options'] as  $layoutOption)
+				<i wire:click="changeLayout('{{$layoutOption['name']}}')" class="{{$layoutOption['icon']}} mx-1 cursor-pointer {{$mainLayout==$layoutOption['name'] ? 'active text-primary' : $layoutOption['name']}}" aria-hidden="true"></i>
+			@endforeach
+		@endif
 	</div>
 	
 </div>
