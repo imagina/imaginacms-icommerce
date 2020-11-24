ee3206c7b9743b7214dbf53f161a827fc081b673
msolanogithub <msolano2505@gmail.com>
2020-11-24 10:08:37 -0500

    +Add: Icommerce | Component "Ribbon" to show products ribbon to Discount, sold out and new product

--- Resources/views/frontend/product/image.blade.php
+++ Resources/views/frontend/product/image.blade.php
@@ -1,19 +1,4 @@
-<div class="bg-img d-flex justify-content-center align-items-center overflow-hidden" style="position: relative">
-  <!--Ribbon discount-->
-  @if($product->discount)
-    <div id="ribbonFeatured">
-      <div id="asideRibbon">
-        @if($product->discount->criteria == 'fixed')
-          <b><i class="fa fa-tags"></i></b>
-        @else
-          <b>{{$product->discount->discount}}%</b>
-        @endif
-        <div class="labelAsideRibbon">DTO.</div>
-      </div>
-    </div>
-  @endif
-
-
+<div class="bg-img d-flex justify-content-center align-items-center overflow-hidden">
 	<a href="{{$product->url}}" title="{{$product->name}}">
 
 		<figure>
@@ -44,33 +29,3 @@
 
 	</a>
 </div>
-
-<style>
-  #ribbonFeatured {
-    width: max-content;
-    position: absolute;
-    top: 5px;
-    right: 0;
-  }
-  #ribbonFeatured #asideRibbon{
-    position: relative;
-    cursor: pointer;
-    padding: 8px 10px 8px 6px;
-    background-color: #fff200;
-    line-height: 1.1;
-  }
-  #ribbonFeatured #asideRibbon::after{
-    content: '';
-    position: absolute;
-    height: 0;
-    width: 0;
-    top: 0;
-    border-style: solid;
-    border-width: 22px 22px 22px 0;
-    border-color: transparent #fff200;
-    left: -22px;
-  }
-  #ribbonFeatured #asideRibbon .labelAsideRibbon{
-    font-size: 10px;
-  }
-</style>
