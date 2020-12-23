810817bd95c4aad1764022eb39770e2fd9d7e532
Juan Escalante <jcec007@gmail.com>
2020-12-23 15:55:22 -0500

    +Add: addToCartWithQuantity.blade.php | blade partial

--- Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-2/infor.blade.php
+++ Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-2/infor.blade.php
@@ -69,63 +69,7 @@
     
     </div>
     @if($addToCartWithQuantity)
-      @if($product->price>0 && $product->stock_status && $product->quantity)
-        <div class="row add-to-cart-with-quantity">
-          <!-- BUTTON QUANTITY -->
-          <div class="number-input input-group quantity-selector">
-            <input type="button" value="-" class="button-minus" data-field="quantity">
-            <input type="number" step="1" max="" value="1" name="quantity" class="quantity-field"
-                   id="quantityField{{$product->id}}">
-            <input type="button" value="+" class="button-plus" data-field="quantity">
-          </div>
-        
-          <!-- BUTTON ADD -->
-          <div class="add-to-cart-button">
-            <a onClick="addToCart({{$product->id}})" class="btn btn-primary">
-              <i class="fa fa-shopping-cart"></i>Comprar
-            </a>
-          </div>
-      
-        </div>
-      @endif
+      @include("icommerce::frontend.components.product.addToCartWithQuantity")
     @endif
   </div>
 </div>
-
-
-@section('scripts')
-  @parent
-  <script>
-    
-    
-    function addToCart() {
-      let quantity = $('#quantityField' + productId).val();
-      window.livewire.emit('addToCart', {{$product->id}}, quantity)
-      $('#quantityField{{$product->id}}' ).val(1)
-    }
-    
-    $('.input-group{{$product->id}}').on('click', '.button-plus-{{$product->id}}', function (e) {
-      e.preventDefault();
-      var currentVal = parseInt($('#quantityField{{$product->id}}').val(), 10);
-      
-      if (!isNaN(currentVal)) {
-        $('#quantityField{{$product->id}}').val(currentVal + 1);
-      } else {
-        $('#quantityField{{$product->id}}').val(0);
-      }
-    });
-    
-    $('.input-group{{$product->id}}').on('click', '.button-minus-{{$product->id}}', function (e) {
-      e.preventDefault();
-      var currentVal = parseInt($('#quantityField{{$product->id}}').val(), 10);
-      
-      if (!isNaN(currentVal) && currentVal > 0) {
-        $('#quantityField{{$product->id}}').val(currentVal - 1);
-      } else {
-        $('#quantityField{{$product->id}}').val(1);
-      }
-    });
-  
-  </script>
-
-@stop
--- Resources/views/vue/components.js
+++ Resources/views/vue/components.js
@@ -1,2 +1,3 @@
 Vue.component('selectProductOptions', require('./components/icommerce/productOptions/selectProductOptions.vue').default);
 Vue.component('featured', require('./components/icommerce/products/featured.vue').default);
+
