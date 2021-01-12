943ca4e18ee69e7ff17bcc46bd2a9542fdcdcf81
Juan Escalante <jcec007@gmail.com>
2021-01-12 09:33:31 -0500

    +Update: change cols in the  product list item views, in the one item by row

--- Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-1/index.blade.php
+++ Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-1/index.blade.php
@@ -7,7 +7,7 @@
   @if(isset($productListLayout) && $productListLayout=='one')
     <div class="row product-list-layout-one">
       
-      <div class="col-12 col-sm-6">
+      <div class="col-6">
         <div class="row justify-content-center position-relative m-0">
           @include('icommerce::frontend.components.product.ribbon')
           <div class="bg-img d-flex justify-content-center align-items-center overflow-hidden">
@@ -16,7 +16,7 @@
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-6">
+      <div class="col-6">
         @include('icommerce::frontend.components.product.product-list-item.layouts.product-list-item-layout-1.infor')
       </div>
     </div>
