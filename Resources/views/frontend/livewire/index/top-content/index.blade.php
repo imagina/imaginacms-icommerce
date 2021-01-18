82c1af9333a5ca46078150433e2e246448867e93
William Verde <williamverde10@gmail.com>
2021-01-18 17:47:03 -0400

    +Update: class on top content default

--- Resources/views/frontend/livewire/index/top-content/index.blade.php
+++ Resources/views/frontend/livewire/index/top-content/index.blade.php
@@ -1,4 +1,4 @@
-<div class="top-content">
+<div class="top-content mb-2">
 	@php($customIndexTitle = setting('icommerce::customIndexTitle'))
 	<h1 class="text-primary text-uppercase font-weight-bold h3">{{$category->title ?? (!empty($customIndexTitle) ? setting('icommerce::customIndexTitle') : trans('icommerce::products.title.products') )}} {{isset($manufacturer->id) ? isset($category->id) ? "/ $manufacturer->name" : $manufacturer->name : ""}}</h1>
 	@if(isset($category->options->descriptionIndex) && !empty($category->options->descriptionIndex))
