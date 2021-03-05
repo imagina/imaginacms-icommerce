89d6db8a79b082c4a7b022edcfb685051bd92751
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-03-05 15:46:06 -0500

    +UPDATE: productable trait functions and accessors

--- Events/Handlers/HandleProductable.php
+++ Events/Handlers/HandleProductable.php
@@ -10,35 +10,26 @@ class HandleProductable
 {
     public function handle($event = null, $data = [])
     {
-        $this->handleSingleProductable($event);
-        $this->handleMultipleProductable($event);
-    }
-
-    public function handleSingleProductable($event){
         $entity = $event->getEntity();
-        $productId = Arr::get($event->getSubmissionData(), 'product_id', null);
-        if(!empty($productId)){
-            $entity->products()->sync([
-                $productId => [
-                    'productable_type' => get_class($entity),
-                ]
-            ]);
+        $productable = Arr::get($event->getSubmissionData(), 'productable', null);
+        if(!empty($productable)){
+            if(is_array($productable)) {
+                $productablesToSync = [];
+                foreach ($productable as $product){
+                    $productablesToSync[$product] = [
+                        'productable_type' => get_class($entity),
+                    ];
+                }
+                $entity->products()->sync($productablesToSync);
+            }else{
+                $entity->products()->sync([
+                    $productable => [
+                        'productable_type' => get_class($entity),
+                    ]
+                ]);
+            }
         }else{
             $entity->products()->sync([]);
-        }
-    }
-
-    public function handleMultipleProductable($event)
-    {
-        $entity = $event->getEntity();
-        $productIds = Arr::get($event->getSubmissionData(), 'product_ids', []);
-        $syncList = [];
-        if (count($productIds)){
-            foreach ($productIds as $productId) {
-                $syncList[$productId] = [];
-                $syncList[$productId]['productable_type'] = get_class($entity);
-            }
-            $entity->products()->sync($syncList);
         }
     }
 
