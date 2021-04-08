7344144e7d75796b724b7566afbc7ca5b4f8c05e
Juan Escalante <jcec007@gmail.com>
2021-04-08 13:01:41 -0500

    +Update: custom itemsList description added

--- Resources/views/frontend/index.blade.php
+++ Resources/views/frontend/index.blade.php
@@ -81,6 +81,7 @@
             :itemComponentAttributes="['category' => $category ?? null, 'manufacturer' => $manufacturer ?? null]"
             entityName="Product"
             :title="(isset($category->id) ? $category->title : '').(isset($manufacturer->id) ? (isset($category->id) ? ' / ' : '').$manufacturer->name : '')"
+            :description="isset($category->options->descriptionIndex) && !empty($category->options->descriptionIndex) ? $category->options->descriptionIndex : null "
             :params="[
             'filter' => ['category' => $category->id ?? null, 'manufacturers' => isset($manufacturer->id) ? [$manufacturer->id] : []],
             'include' => ['discounts','translations','category','categories','manufacturer','productOptions'], 
