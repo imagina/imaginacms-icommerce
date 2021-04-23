6a4b5de74875f6b2d42d5277a96e7e71faa6d1ad
Juan Escalante <jcec007@gmail.com>
2021-04-23 12:03:57 -0500

    +Add: new Cart layout 3

---
+++ Resources/views/frontend/livewire/cart/layouts/cart-button-layout-3/button.blade.php
@@ -0,0 +1,16 @@
+<a class="cart-link"  data-toggle="modal" data-target="#modalCart">
+  
+  <div class="cart">
+   
+    <span class="quantity">
+       @if(!isset($cart->quantity))
+        <i class="fa fa-spinner fa-pulse fa-fw"></i>
+      @else
+        {{  $cart->quantity  }}
+      @endif
+    </span>
+    
+    <i class="{{$icon}}"></i>
+  </div>
+
+</a>
