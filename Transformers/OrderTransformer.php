1d252805494bb76b5a893b307bdc81b913701cd4
Juan Escalante <jcec007@gmail.com>
2021-10-06 16:47:08 -0500

    +Update: validate payment and shipping Telephones if exist

--- Transformers/OrderTransformer.php
+++ Transformers/OrderTransformer.php
@@ -159,7 +159,7 @@ class OrderTransformer extends JsonResource
             ],
             [
               'label' => trans("iprofile::frontend.form.cellularPhone"),
-              'value' => "{$item['shippingTelephone']}"
+              'value' => $item['shippingTelephone'] ?? ''
             ]
           
           ]
@@ -218,7 +218,7 @@ class OrderTransformer extends JsonResource
           ],
           [
             'label' => trans("iprofile::frontend.form.cellularPhone"),
-            'value' => "{$item['paymentTelephone']}"
+            'value' => $item['paymentTelephone'] ?? ''
           ]
         ]
       ];
