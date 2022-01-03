19b375d6bb1c49a3e84beb6614222e70c74c9811
William Verde <williamverde10@gmail.com>
2022-01-03 18:30:44 -0400

    Update: translation point show product

--- Resources/lang/es/products.php
+++ Resources/lang/es/products.php
@@ -67,7 +67,7 @@ return [
         'certificate'               => 'Certificado',
         'data_sheet'                => 'Ficha técnica',
         'points'                    => 'Puntos',
-        'points win'                => 'Gana :points Puntos por comprar este producto',
+        'points win'                => 'Puntos para comprar este producto: :points',
     ],
     'categories'        => [
         'free_shipping'             => 'Productos con envío gratuito',
