3f7d711d02c8870c32a7c47b424592dd4d89dc2f
Juan Escalante <jcec007@gmail.com>
2022-02-07 11:21:16 -0500

    +Update: SendOrder and UpdateOrderStatus handlers | validate if there area roles with tenant configuration then the notifiaction must be sent only of the child orders

--- Events/Handlers/SendOrder.php
+++ Events/Handlers/SendOrder.php
@@ -34,44 +34,70 @@ class SendOrder
       
       //Subject
       $subject = trans('icommerce::orders.messages.purchase order') . " #" . $order->id . ": " . $order->status->title;
+  
+      //Roles to tenant
+      $rolesToTenant = json_decode(setting("isite::rolesToTenant",null,"[]"));
+  
+      // si hay roles asignados a funcionar como tenant entonces el customer de la orden debe ser notificado sólo en las
+      // ordenes hijas
+      \Log::info('Icommerce: Events|Handlers|SendOrder|RolesToTenant: '.json_encode($rolesToTenant));
+      if(!empty($rolesToTenant)){
+        // only insert the customer if the order has a parentId
+        if(!is_null($order->parent_id)){
+          list($emailTo,$users) = $this->getUsersAndEmails($order);
+        }
+      }else{
+        list($emailTo,$users) = $this->getUsersAndEmails($order);
+      }
       
-      //Emails from setting form-emails
-      $emailTo = json_decode(setting("icommerce::form-emails", null, "[]"));
-      if (empty($emailTo)) //validate if its a string separately by commas
-        $emailTo = explode(',', setting('icommerce::form-emails'));
+      //if there are valid and not empty emailsTo
+      if(isset($emailTo) && !empty($emailTo)){
+  
+        //send notification by email, broadcast and push -- by default only send by email
+        $this->notificationService->to([
+          "email" => $emailTo,
+          "broadcast" => $users->pluck('id')->toArray(),
+          "push" => $users->pluck('id')->toArray(),
+        ])->push(
+          [
+            "title" => trans("icommerce::orders.title.confirmation_single_order_title"),
+            "message" => $subject,
+            "icon_class" => "fas fa-shopping-cart",
+            "link" => $order->url,
+            "content" => "icommerce::emails.order",
+            "view" => "icommerce::emails.Order",
+            "setting" => [
+              "saveInDatabase" => 1 // now, the notifications with type broadcast need to be save in database to really send the notification
+            ],
+            "order" => $order
+          ]
+        );
+        
+      }
       
-      //Emails from users selected in the setting usersToNotify
-      $usersToNotify = json_decode(setting("icommerce::usersToNotify", null, "[]"));
-      $users = User::whereIn("id", $usersToNotify)->get();
-      $emailTo = array_merge($emailTo, $users->pluck('email')->toArray());
-      
-      //By last, gets the Email of the user in the order
-      array_push($emailTo, $order->email);
-   
-      //send notification by email, broadcast and push -- by default only send by email
-      $this->notificationService->to([
-        "email" => $emailTo,
-        "broadcast" => $users->pluck('id')->toArray(),
-        "push" => $users->pluck('id')->toArray(),
-      ])->push(
-        [
-          "title" => trans("icommerce::orders.title.confirmation_single_order_title"),
-          "message" => $subject,
-          "icon_class" => "fas fa-shopping-cart",
-          "link" => $order->url,
-          "content" => "icommerce::emails.order",
-          "view" => "icommerce::emails.Order",
-          "setting" => [
-            "saveInDatabase" => 1 // now, the notifications with type broadcast need to be save in database to really send the notification
-          ],
-          "order" => $order
-        ]
-      );
       
     } catch (\Exception $e) {
-      \Log::error("Error | SendOrder Event: " . $e->getMessage() . "\n" . $e->getFile() . "\n" . $e->getLine() . $e->getTraceAsString());
+      \Log::error('Icommerce: Events|Handlers|SendOrder|Message: '.$e->getMessage().' | FILE: '.$e->getFile().' | LINE: '.$e->getLine());
     }
     
+  }
+  
+  private function getUsersAndEmails($order){
+  
+    //Emails from setting form-emails
+    $emailTo = json_decode(setting("icommerce::form-emails", null, "[]"));
+    if (empty($emailTo)) //validate if its a string separately by commas
+      $emailTo = explode(',', setting('icommerce::form-emails'));
+  
+    //Emails from users selected in the setting usersToNotify
+    $usersToNotify = json_decode(setting("icommerce::usersToNotify", null, "[]"));
+    $users = User::whereIn("id", $usersToNotify)->get();
+    $emailTo = array_merge($emailTo, $users->pluck('email')->toArray());
+  
+    //By last, gets the Email of the user in the order
+    array_push($emailTo, $order->email);
+  
+    return [$emailTo, $users];
   }
   
   
--- Events/Handlers/UpdateOrderStatus.php
+++ Events/Handlers/UpdateOrderStatus.php
@@ -37,45 +37,66 @@ class UpdateOrderStatus
       if (isset($data) && $data['notify']) {
         $order = $order->first();
         
-        //Emails from setting form-emails
-        $emailTo = json_decode(setting("icommerce::form-emails", null, "[]"));
-        if (empty($emailTo)) //validate if its a string separately by commas
-          $emailTo = explode(',', setting('icommerce::form-emails'));
+        //Roles to tenant
+        $rolesToTenant = json_decode(setting("isite::rolesToTenant", null, "[]"));
         
-        //Emails from users selected in the setting usersToNotify
-        $usersToNotity = json_decode(setting("icommerce::usersToNotify", null, "[]"));
-        $users = User::whereIn("id", $usersToNotity)->get();
-        $emailTo = array_merge($emailTo, $users->pluck('email')->toArray());
+        // si hay roles asignados a funcionar como tenant entonces el customer de la orden debe ser notificado sólo en las
+        // ordenes hijas
+        if (!empty($rolesToTenant)) {
+          // only insert the customer if the order has a parentId
+          if (!is_null($order->parent_id)) {
+            list($emailTo, $users) = $this->getUsersAndEmails($order);
+          }
+        } else {
+          list($emailTo, $users) = $this->getUsersAndEmails($order);
+        }
         
-        //By last, gets the Email of the user in the order
-        array_push($emailTo, $order->email);
-        
-        //send notification by email, broadcast and push -- by default only send by email
-        $this->notification->to([
-          "email" => $emailTo,
-          "broadcast" => $users->pluck('id')->toArray(),
-          "push" => $users->pluck('id')->toArray(),
-        ])->push(
-          [
-            "title" => trans("icommerce::orders.messages.notificationOrder") . " #" . $order->id,
-            "message" => trans("icommerce::orders.messages.statusChanged", ["orderId" => $order->id, "statusName" => $order->status->title]),
-            "icon_class" => "fas fa-shopping-cart",
-            "link" => $order->url,
-            "comment" => $data['comment'],
-            "content" => "icommerce::emails.order-notifications",
-            "setting" => [
-              "saveInDatabase" => 1 // now, the notifications with type broadcast need to be save in database to really send the notification
-            ],
-            "order" => $order
-          ]
-        );
-        
+        if (isset($emailTo) && !empty($emailTo)) {
+          //send notification by email, broadcast and push -- by default only send by email
+          $this->notification->to([
+            "email" => $emailTo,
+            "broadcast" => $users->pluck('id')->toArray(),
+            "push" => $users->pluck('id')->toArray(),
+          ])->push(
+            [
+              "title" => trans("icommerce::orders.messages.notificationOrder") . " #" . $order->id,
+              "message" => trans("icommerce::orders.messages.statusChanged", ["orderId" => $order->id, "statusName" => $order->status->title]),
+              "icon_class" => "fas fa-shopping-cart",
+              "link" => $order->url,
+              "comment" => $data['comment'],
+              "content" => "icommerce::emails.order-notifications",
+              "setting" => [
+                "saveInDatabase" => 1 // now, the notifications with type broadcast need to be save in database to really send the notification
+              ],
+              "order" => $order
+            ]
+          );
+          
+        }
       }
       
     } catch (\Exception $e) {
       \Log::error("Error | UpdateOrderStatus Event: " . $e->getMessage() . "\n" . $e->getFile() . "\n" . $e->getLine());
     }
     
+  }
+  
+  private function getUsersAndEmails($order)
+  {
+    //Emails from setting form-emails
+    $emailTo = json_decode(setting("icommerce::form-emails", null, "[]"));
+    if (empty($emailTo)) //validate if its a string separately by commas
+      $emailTo = explode(',', setting('icommerce::form-emails'));
+    
+    //Emails from users selected in the setting usersToNotify
+    $usersToNotity = json_decode(setting("icommerce::usersToNotify", null, "[]"));
+    $users = User::whereIn("id", $usersToNotity)->get();
+    $emailTo = array_merge($emailTo, $users->pluck('email')->toArray());
+    
+    //By last, gets the Email of the user in the order
+    array_push($emailTo, $order->email);
+    
+    return [$emailTo, $users];
   }
   
 }
