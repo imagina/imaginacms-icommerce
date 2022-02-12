9ae0b2086130c0cd7a678e90749fb3d7c32e3669
Michael <msolano@imaginacolombia.com>
2022-02-12 12:59:44 -0500

    +Add: hasEventsWithBindings trait

--- Entities/PaymentMethod.php
+++ Entities/PaymentMethod.php
@@ -4,21 +4,22 @@ namespace Modules\Icommerce\Entities;
 
 use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Core\Icrud\Traits\hasEventsWithBindings;
 use Modules\Media\Support\Traits\MediaRelation;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 use Modules\Iforms\Support\Traits\Formeable;
 
 class PaymentMethod extends Model
 {
-  use Translatable, MediaRelation, Formeable, BelongsToTenant;
-  
+  use Translatable, MediaRelation, Formeable, BelongsToTenant, hasEventsWithBindings;
+
   public $translatedAttributes = [
     'title',
     'description'
   ];
-  
+
   protected $table = 'icommerce__payment_methods';
-  
+
   protected $fillable = [
     'status',
     'name',
@@ -28,14 +29,14 @@ class PaymentMethod extends Model
     'geozone_id',
     'parent_name'
   ];
-  
+
 
   protected $casts = [
     'options' => 'array'
   ];
-  
+
   public $tenantWithCentralData = false;
-  
+
   public function __construct(array $attributes = [])
   {
     try{
@@ -44,10 +45,10 @@ class PaymentMethod extends Model
         $this->tenantWithCentralData = in_array("paymentMethods", $entitiesWithCentralData);
       }
     }catch(\Exception $e){}
-    
+
     parent::__construct($attributes);
   }
-  
+
   public function store()
   {
     if (is_module_enabled('Marketplace')) {
@@ -55,23 +56,23 @@ class PaymentMethod extends Model
     }
     return $this->belongsTo(Store::class);
   }
-  
+
   public function getOptionsAttribute($value)
   {
     return json_decode($value);
   }
-  
-  
+
+
   public function transactions()
   {
     return $this->hasMany(Transaction::class);
   }
-  
+
   public function geozones()
   {
     return $this->belongsToMany('Modules\Ilocations\Entities\Geozones', 'icommerce__payment_methods_geozones', 'payment_method_id', 'geozone_id')->withTimestamps();
   }
-  
+
   public function getMainImageAttribute()
   {
     $thumbnail = $this->files()->where('zone', 'mainimage')->first();
