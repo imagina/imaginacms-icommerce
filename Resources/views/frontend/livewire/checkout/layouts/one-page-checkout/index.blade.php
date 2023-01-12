ddd163ed46a71571bbda3bb23a196b633788f534
T-JULIAND <juliandavidbonillatorres@gmail.com>
2023-01-12 12:16:38 -0500

    +Fix : Back-End | config the columns and order card for layout 'onePageCheckout', and number position info card +Fix : Back-End | validate colum of index checkout

--- Resources/views/frontend/livewire/checkout/layouts/one-page-checkout/index.blade.php
+++ Resources/views/frontend/livewire/checkout/layouts/one-page-checkout/index.blade.php
@@ -6,38 +6,39 @@
 </div>
 
 <div class="row">
-  
-  <div class="col-12 col-md-6 col-lg-4">
-    @include('icommerce::frontend.livewire.checkout.layouts.one-page-checkout.customer')
-  </div>
-  
-  <div class="col-12 col-md-6 col-lg-4">
-    @include('icommerce::frontend.livewire.checkout.layouts.one-page-checkout.billing-details')
-    
-    @php($step = 3)
-    @if($requireShippingMethod)
-      @include('icommerce::frontend.livewire.checkout.layouts.one-page-checkout.shipping-details')
-      @php($step++)
-    @endif
-  </div>
-  
-  <div class="col-12 col-md-12 col-lg-4">
-    
-    @if($requireShippingMethod)
-      @include('icommerce::frontend.livewire.checkout.layouts.one-page-checkout.shipping-methods')
-      @php($step++)
-    @endif
-    
-    @include('icommerce::frontend.livewire.checkout.layouts.one-page-checkout.payment-methods')
-    @include('icommerce::frontend.livewire.checkout.partials.order-summary')
-  
-  </div>
+  @php($columns = config('asgard.icommerce.config.onePageCheckout.columns'))
+  @foreach($columns as $colum)
+    <div class="{{$colum['class']}}">
+      @foreach($colum['cards'] as $card)
+        @if($card == 'order-summary')
+          @include('icommerce::frontend.livewire.checkout.partials.order-summary')
+        @else
+          @include('icommerce::frontend.livewire.checkout.layouts.one-page-checkout.' . $card)
+        @endif
+      @endforeach
+    </div>
+  @endforeach
+
 
 </div>
-
 <div class="row">
   <div class="col py-2">
     <a class="btn btn-primary" href="{{url('/')}}">{{ trans('icommerce::checkout.continue_buying') }}</a>
   </div>
 </div>
 
+
+{{--<script>--}}
+{{--  $(document).ready(function () {--}}
+{{--    $colum1 = document.querySelector('.colum-1');--}}
+{{--    $cardPaymentAddress = document.querySelector('#cardPaymentAddress');--}}
+{{--    $colum1.appendChild($cardPaymentAddress)--}}
+
+{{--    $colum2 = document.querySelector('.colum-2');--}}
+{{--    $cardShippingMethods = document.querySelector('#cardShippingMethods');--}}
+{{--    $colum2.appendChild($cardShippingMethods)--}}
+
+{{--    $cardPaymentMethods = document.querySelector('#cardPaymentMethods');--}}
+{{--    $colum2.appendChild($cardPaymentMethods)--}}
+{{--  })--}}
+{{--</script>--}}
