a625fc7c6b90a01ff5dbf1e09d2f3e88d1fa1afe
msolanogithub <msolano@imaginacolombia.com>
2023-07-13 16:11:59 -0500

    Add: getCategories method to the IcommerceContentIa services

--- Services/IcommerceContentAi.php
+++ Services/IcommerceContentAi.php
@@ -17,11 +17,27 @@ class IcommerceContentAi
     $this->aiService = new AiService();
   }
 
+  public function getCategories($quantity = 2)
+  {
+    \Log::info($this->log . "getCategories|INIT");
+    //instance the prompt to generate the categories
+    $prompt = "Información descriptiva para categorias de productos usados en un sitio WEB-ecommerce con los siguientes atributos ";
+    //Instance attributes
+    $prompt .= $this->aiService->getStandardPrompts(
+      ["title", "description", "slug"]
+    );
+    //Call IA Service
+    $response = $this->aiService->getContent($prompt, $quantity);
+    \Log::info($this->log . "getCategories|END");
+    //Return response
+    return $response;
+  }
+
   public function getProducts($quantity = 2)
   {
-    \Log::info($this->log."getProducts|INIT");
-    //instance the prompt to generate the posts
-    $prompt = "Información descriptiba de productos usados en un sitio WEB-ecommerce con los siguientes atributos ";
+    \Log::info($this->log . "getProducts|INIT");
+    //instance the prompt to generate the products
+    $prompt = "Información descriptiva de productos usados en un sitio WEB-ecommerce con los siguientes atributos ";
     //Instance attributes
     $prompt .= $this->aiService->getStandardPrompts(
       ["name", "description", "summary", "slug", "category_id", "price"],
@@ -29,7 +45,7 @@ class IcommerceContentAi
     );
     //Call IA Service
     $response = $this->aiService->getContent($prompt, $quantity);
-    \Log::info($this->log."getProducts|END");
+    \Log::info($this->log . "getProducts|END");
     //Return response
     return $response;
   }
