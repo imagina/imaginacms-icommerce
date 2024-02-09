8ae5b9e1a888fea5271d7d8e1a27cfcf121bedff
William Verde <williamverde10@gmail.com>
2024-02-09 17:13:06 -0400

    Added: repositories, cache etc

---
+++ Repositories/Eloquent/EloquentProductOptionValueWarehouseRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Modules\Icommerce\Repositories\Eloquent;
+
+use Modules\Icommerce\Repositories\ProductOptionValueWarehouseRepository;
+use Modules\Core\Icrud\Repositories\Eloquent\EloquentCrudRepository;
+
+class EloquentProductOptionValueWarehouseRepository extends EloquentCrudRepository implements ProductOptionValueWarehouseRepository
+{
+  /**
+   * Filter names to replace
+   * @var array
+   */
+  protected $replaceFilters = [];
+
+  /**
+   * Relation names to replace
+   * @var array
+   */
+  protected $replaceSyncModelRelations = [];
+
+  /**
+   * Attribute to define default relations
+   * all apply to index and show
+   * index apply in the getItemsBy
+   * show apply in the getItem
+   * @var array
+   */
+  protected $with = [/*all => [] ,index => [],show => []*/];
+
+  /**
+   * Filter query
+   *
+   * @param $query
+   * @param $filter
+   * @param $params
+   * @return mixed
+   */
+  public function filterQuery($query, $filter, $params)
+  {
+
+    /**
+     * Note: Add filter name to replaceFilters attribute before replace it
+     *
+     * Example filter Query
+     * if (isset($filter->status)) $query->where('status', $filter->status);
+     *
+     */
+
+    //Response
+    return $query;
+  }
+
+  /**
+   * Method to sync Model Relations
+   *
+   * @param $model ,$data
+   * @return $model
+   */
+  public function syncModelRelations($model, $data)
+  {
+    //Get model relations data from attribute of model
+    $modelRelationsData = ($model->modelRelations ?? []);
+
+    /**
+     * Note: Add relation name to replaceSyncModelRelations attribute before replace it
+     *
+     * Example to sync relations
+     * if (array_key_exists(<relationName>, $data)){
+     *    $model->setRelation(<relationName>, $model-><relationName>()->sync($data[<relationName>]));
+     * }
+     *
+     */
+
+    //Response
+    return $model;
+  }
+}
