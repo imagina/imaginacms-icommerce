2bbc414c407f9b4865f459854b5b9e6f5ec94e52
William Verde <williamverde10@gmail.com>
2024-02-09 17:14:47 -0400

    Added: new translations cases warehouse

---
+++ Resources/lang/es/warehouses.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'single' => 'Bodega',
+    'plural' => 'Bodegas',
+    'list resource' => 'List warehouses',
+    'create resource' => 'Create warehouses',
+    'edit resource' => 'Edit warehouses',
+    'destroy resource' => 'Destroy warehouses',
+    'title' => [
+        'warehouses' => 'Warehouse',
+        'create warehouse' => 'Create a warehouse',
+        'edit warehouse' => 'Edit a warehouse',
+    ],
+    'button' => [
+        'create warehouse' => 'Create a warehouse',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+        'warehouse not found for your address long' => 'Tu dirección no tiene cobertura para Domicilio, debes cargar otra dirección o Recoger en Tienda la bodega más cercana a tu domicilio'
+    ],
+    'validation' => [
+    ],
+];
