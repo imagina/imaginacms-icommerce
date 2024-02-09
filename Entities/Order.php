e8cd5c23cf8144b6f60be1af2446d5bb32c6f085
William Verde <williamverde10@gmail.com>
2024-02-09 17:21:37 -0400

    Added: attributes warehouse in Order entity

--- Entities/Order.php
+++ Entities/Order.php
@@ -85,7 +85,9 @@ class Order extends Model
     'organization_id',
     'suscription_token',
     'type',
-    'guest_purchase'
+    'guest_purchase',
+    'warehouse_id',
+    'warehouse_title'
   ];
   
   
