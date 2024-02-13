a44e8bfc5aae233d9742dc05429c59e68df06d37
William Verde <williamverde10@gmail.com>
2024-02-13 12:34:08 -0400

    Added: warehouses translations

--- Resources/lang/es/warehouses.php
+++ Resources/lang/es/warehouses.php
@@ -14,13 +14,21 @@ return [
     ],
     'button' => [
         'create warehouse' => 'Create a warehouse',
+        'keep' => 'Mantener',
+        'change' => 'Cambiar',
     ],
     'table' => [
     ],
     'form' => [
     ],
     'messages' => [
-        'warehouse not found for your address long' => 'Tu dirección no tiene cobertura para Domicilio, debes cargar otra dirección o Recoger en Tienda la bodega más cercana a tu domicilio'
+        'warehouse not found for your address long' => 'Tu dirección no tiene cobertura para Domicilio, debes cargar otra dirección o Recoger en Tienda la bodega más cercana a tu domicilio',
+        'hello' => 'Hola',
+        'your address is' => 'Tu ubicación es:',
+        'buying for' => 'Estas comprando para:',
+        'not logged' => 'Iniciar sesión para usar tus direcciones guardadas',
+        'you are going to buy for' => '¿Vas a comprar para:',
+        'products for this location' => 'Te mostraremos los productos disponibles para esta localidad'
     ],
     'validation' => [
     ],
