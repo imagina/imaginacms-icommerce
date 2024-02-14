070091cf35fd79321ee74e40d602c9c11e2b97f9
William Verde <williamverde10@gmail.com>
2024-02-14 15:30:22 -0400

    Added: validation user logged in tab delivery

--- Resources/views/frontend/livewire/warehouse-locator/layouts/tabs/tab-delivery.blade.php
+++ Resources/views/frontend/livewire/warehouse-locator/layouts/tabs/tab-delivery.blade.php
@@ -1,36 +1,46 @@
 <div class="tab-pane fade {{$tabSelected== $shippingMethods['delivery'] ? 'show active' : ''}} " 
 id="homeModalWarehouseLocator" role="tabpanel" aria-labelledby="homeTabModalWarehouseLocatorab">
 
-    @if(count($userShippingAddresses)==0 || $showAddressForm)
+    @if(is_null($user))
 
-        <livewire:iprofile::address-form 
-            :embedded="true" 
-            type="shipping"  
-            key="checkoutShippingAddress" 
-            :openInModal="false" 
-            :insideModal="true"
-            :showCancelBtn="true"
-            :userAddresses="$userShippingAddresses"/>
+        {{trans('icommerce::warehouses.messages.hello')}}, 
+        <a class="cursor-pointer text-primary" onclick="location.href='{{"/ipanel/#/auth/login/?redirectTo=".url()->current()}}'">
+            {{trans('icommerce::warehouses.messages.not logged')}}
+        </a>
+
     @else
-        
-        <livewire:iprofile::address-list :addresses="$userShippingAddresses" :addressSelected="$shippingAddress" emit="shippingAddressChanged"/>
-        
-        {{-- BTN NEW ADDRESS --}}
-        <div class="form-row justify-content-center mt-4">
 
-            <div class="form-group col-md-6">
-                <button wire:click="$set('showAddressForm', true)" type="button" class="btn btn-primary btn-block">
-                    {{trans('icommerce::warehouses.button.add new address')}}
-                </button>
+        @if(count($userShippingAddresses)==0 || $showAddressForm)
+
+            <livewire:iprofile::address-form 
+                :embedded="true" 
+                type="shipping"  
+                key="checkoutShippingAddress" 
+                :openInModal="false" 
+                :insideModal="true"
+                :showCancelBtn="true"
+                :userAddresses="$userShippingAddresses"/>
+        @else
+            
+            <livewire:iprofile::address-list :addresses="$userShippingAddresses" :addressSelected="$shippingAddress" emit="shippingAddressChanged"/>
+            
+            {{-- BTN NEW ADDRESS --}}
+            <div class="form-row justify-content-center mt-4">
+
+                <div class="form-group col-md-6">
+                    <button wire:click="$set('showAddressForm', true)" type="button" class="btn btn-primary btn-block">
+                        {{trans('icommerce::warehouses.button.add new address')}}
+                    </button>
+                </div>
+
+                <div class="form-group col-md-6">
+                    @include('icommerce::frontend.livewire.warehouse-locator.layouts.tabs.btn-confirm')
+                </div>
+
             </div>
 
-            <div class="form-group col-md-6">
-                @include('icommerce::frontend.livewire.warehouse-locator.layouts.tabs.btn-confirm')
-            </div>
-
-        </div>
-
-    @endif
+        @endif
    
+    @endif
    
 </div>
