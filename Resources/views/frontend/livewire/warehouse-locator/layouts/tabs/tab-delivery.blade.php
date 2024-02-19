15240faa42590cdabee60699b5ba9da260d842db
William Verde <williamverde10@gmail.com>
2024-02-19 15:23:42 -0400

    Update: fix reedirect url when not logged | tab delivery

--- Resources/views/frontend/livewire/warehouse-locator/layouts/tabs/tab-delivery.blade.php
+++ Resources/views/frontend/livewire/warehouse-locator/layouts/tabs/tab-delivery.blade.php
@@ -3,8 +3,9 @@ id="homeModalWarehouseLocator" role="tabpanel" aria-labelledby="homeTabModalWare
 
     @if(is_null($user))
 
+        @php $reedirectUrl = "/ipanel/#/auth/login/?redirectTo=".url('/'); @endphp
         {{trans('icommerce::warehouses.messages.hello')}}, 
-        <a class="cursor-pointer text-primary" onclick="location.href='{{"/ipanel/#/auth/login/?redirectTo=".url()->current()}}'">
+        <a class="cursor-pointer text-primary" onclick="location.href='{{$reedirectUrl}}'">
             {{trans('icommerce::warehouses.messages.not logged')}}
         </a>
 
