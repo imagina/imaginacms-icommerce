162d3dd558e5611b8167a98d25ad29d369d1e24f
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2024-03-14 10:59:48 -0500

    +Add : Back-end | Validate partial custom Theme for product

--- Resources/views/frontend/show.blade.php
+++ Resources/views/frontend/show.blade.php
@@ -1,122 +1,123 @@
 @extends('icommerce::structure.product-show')
 
 @section('content')
-  @parent
-  <div class="page show show-product show-product-{{$product->id}}">
-    <div id="content_show_commerce">
+	@parent
+	<div class="page show show-product show-product-{{$product->id}}">
+		<div id="content_show_commerce">
 
-      {{-- Banner Top--}}
-      @include("icommerce::frontend.partials.banner")
+			{{-- Banner Top--}}
+			@include("icommerce::frontend.partials.banner")
 
-      <div id="content" class="product">
-        <div class="container ">
+			<div id="content" class="product">
+				<div class="container ">
 
-          <div class="row">
+					<div class="row">
 
-            <div class="col-lg-6 mb-5">
+						<div class="col-lg-6 mb-5">
 
-              <!-- Wishlist Modal List -->
-			        @livewire("wishlistable::wishlist",["layout"=>"wishlist-layout-modal-list-1", "item" => $product])
+							<!-- Wishlist Modal List -->
+							@livewire("wishlistable::wishlist",["layout"=>"wishlist-layout-modal-list-1", "item" => $product])
 
-              @include('icommerce::frontend.partials.show.gallery')
+							@include('icommerce::frontend.partials.show.gallery')
 
-              @includeFirst(["icommerce.partials.show.custom-extra-information-bottom-gallery",
-                             "icommerce::frontend.partials.show.custom-extra-information-bottom-gallery"])
+							@includeIf("icommerce.partials.show.custom-extra-information-bottom-gallery")
 
-              @include('icommerce::frontend.partials.show.share')
+							@include('icommerce::frontend.partials.show.share')
 
-            </div>
+						</div>
 
-            <div class="col-lg-6 mb-5">
+						<div class="col-lg-6 mb-5">
 
-              @include('icommerce::frontend.partials.show.information')
-              @includeFirst(["icommerce.partials.show.custom-extra-information","icommerce::frontend.partials.show.custom-extra-information"])
-            </div>
+							@include('icommerce::frontend.partials.show.information')
 
-            <div class="col-lg-12 mb-5">
-              <div class="row">
-                <div class="col-12 ">
-                  <ul class="nav nav-tabs border-left border-right border-top" role="tablist">
-                    <li class="nav-item">
-                      <a class="nav-link active" data-toggle="tab" href="#descripcion"
-                         role="tab">{{ trans('icommerce::products.title.productDetails') }}</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" data-toggle="tab" href="#comentarios"
-                         role="tab">{{ trans('icommerce::products.title.comments') }}</a>
-                    </li>
+							@includeIf("icommerce.partials.show.custom-extra-information")
+						</div>
 
-                    @if(setting('icommerce::showReviewsProduct') && is_module_enabled('Rateable') && is_module_enabled('Icomments'))
-                      <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#reviews"
-                           role="tab">{{ trans('icommerce::products.title.reviews') }}</a>
-                      </li>
-                    @endif
+						<div class="col-lg-12 mb-5">
+							<div class="row">
+								<div class="col-12 ">
+									<ul class="nav nav-tabs border-left border-right border-top" role="tablist">
+										<li class="nav-item">
+											<a class="nav-link active" data-toggle="tab" href="#descripcion"
+												 role="tab">{{ trans('icommerce::products.title.productDetails') }}</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" data-toggle="tab" href="#comentarios"
+												 role="tab">{{ trans('icommerce::products.title.comments') }}</a>
+										</li>
 
-
-                  </ul>
-                </div>
-              </div>
-              <!-- Tab panes -->
-              <div class="row">
-                <div class="col-12">
-                  <div class="tab-content border">
-
-                    <div class="tab-pane active " id="descripcion" role="tabpanel">
-                      <div class="p-3 p-md-5">
-                        @include('icommerce::frontend.partials.show.tabs')
-                      </div>
-                    </div>
-
-                    <div class="tab-pane" id="comentarios" role="tabpanel">
-                      <div class="p-3 p-md-5">
-                        <div class="fb-comments w-100" data-href="{{$product->url}}" data-numposts="5"
-                             data-width="100%"></div>
-                        <div id="fb-root"></div>
-                      </div>
-                    </div>
-
-                    @if(setting('icommerce::showReviewsProduct') && is_module_enabled('Rateable') && is_module_enabled('Icomments'))
-                      <div class="tab-pane" id="reviews" role="tabpanel">
-                        <div class="p-3 p-md-5">
-
-                          @include('icommerce::frontend.partials.show.reviews',['product' => $product])
-
-                        </div>
-                      </div>
-                    @endif
-
-                  </div>
-                </div>
-              </div>
-            </div>
+										@if(setting('icommerce::showReviewsProduct') && is_module_enabled('Rateable') && is_module_enabled('Icomments'))
+											<li class="nav-item">
+												<a class="nav-link" data-toggle="tab" href="#reviews"
+													 role="tab">{{ trans('icommerce::products.title.reviews') }}</a>
+											</li>
+										@endif
 
 
+									</ul>
+								</div>
+							</div>
+							<!-- Tab panes -->
+							<div class="row">
+								<div class="col-12">
+									<div class="tab-content border">
 
-          </div>
-        </div>
-      </div>
+										<div class="tab-pane active " id="descripcion" role="tabpanel">
+											<div class="p-3 p-md-5">
+												@include('icommerce::frontend.partials.show.tabs')
+											</div>
+										</div>
 
-      <div class="col-12">
+										<div class="tab-pane" id="comentarios" role="tabpanel">
+											<div class="p-3 p-md-5">
+												<div class="fb-comments w-100" data-href="{{$product->url}}" data-numposts="5"
+														 data-width="100%"></div>
+												<div id="fb-root"></div>
+											</div>
+										</div>
 
-        @include('icommerce::frontend.partials.show.related-products')
+										@if(setting('icommerce::showReviewsProduct') && is_module_enabled('Rateable') && is_module_enabled('Icomments'))
+											<div class="tab-pane" id="reviews" role="tabpanel">
+												<div class="p-3 p-md-5">
 
-      </div>
+													@include('icommerce::frontend.partials.show.reviews',['product' => $product])
 
-    </div>
+												</div>
+											</div>
+										@endif
 
-    {{-- Extra Footer End Page --}}
-    @include('icommerce::frontend.partials.extra-footer')
+									</div>
+								</div>
+							</div>
+						</div>
 
-  </div>
+
+					</div>
+				</div>
+			</div>
+
+			<div class="col-12">
+
+				@include('icommerce::frontend.partials.show.related-products')
+
+			</div>
+
+		</div>
+
+		{{-- Extra Footer End Page --}}
+		@include('icommerce::frontend.partials.extra-footer')
+
+	</div>
 @stop
 
 @section('scripts')
-  @parent
+	@parent
 
-  @include('icommerce::frontend.partials.style')
-  <script defer type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5fd9384eb64d610011fa8357&product=inline-share-buttons" async="async"></script>
-  <script type="text/javascript" defer>(function (d, s, id) {
+	@include('icommerce::frontend.partials.style')
+	<script defer type="text/javascript"
+					src="https://platform-api.sharethis.com/js/sharethis.js#property=5fd9384eb64d610011fa8357&product=inline-share-buttons"
+					async="async"></script>
+	<script type="text/javascript" defer>(function (d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s);
@@ -124,5 +125,5 @@
       js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
-  </script>
+	</script>
 @stop
