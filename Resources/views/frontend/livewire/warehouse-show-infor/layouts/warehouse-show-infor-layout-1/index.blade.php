fd1a22bcca65d555e3e46a1d72b17288367450dc
William <williamverde10@gmail.com>
2024-07-30 11:40:57 -0400

    Added: loading to Warehouse show infor

--- Resources/views/frontend/livewire/warehouse-show-infor/layouts/warehouse-show-infor-layout-1/index.blade.php
+++ Resources/views/frontend/livewire/warehouse-show-infor/layouts/warehouse-show-infor-layout-1/index.blade.php
@@ -3,5 +3,12 @@
 
         {!! $title!!}
 
+    @else
+
+        <div class="spinner-border spinner-border-sm mr-3" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+
     @endif
+
 </div>
