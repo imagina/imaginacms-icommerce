b795dfc797f13d0b91ef7566c9e623ebe0094fae
William <williamverde10@gmail.com>
2024-07-30 12:17:13 -0400

    Fix: bug livewire with pagination, filters etc

--- Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-1/index.blade.php
+++ Resources/views/frontend/components/product/product-list-item/layouts/product-list-item-layout-1/index.blade.php
@@ -48,7 +48,7 @@
  <!-- Validacion aca porque se reutiliza el mismo componente, con diferentes layouts en varias partes --> 
 @if(setting('wishlistable::wishlistActive',null,false) && wishlistableShowBtn($currentRoute))
   <!-- Wishlist Modal List -->
-  @livewire("wishlistable::wishlist",["layout"=>"wishlist-layout-modal-list-1","layoutButton"=>'btn', "item" => $product])
+  @livewire("wishlistable::wishlist",["layout"=>"wishlist-layout-modal-list-1","layoutButton"=>'btn', "item" => $product],key(uniqid()))
 @endif
 
  
