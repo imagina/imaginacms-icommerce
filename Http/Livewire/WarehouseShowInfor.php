f60224320b364df2c09d848a25a9532fd269ca66
William <williamverde10@gmail.com>
2024-09-10 15:20:03 -0400

    Update: validation login user | Warehouse Show Infor | From v8x.

--- Http/Livewire/WarehouseShowInfor.php
+++ Http/Livewire/WarehouseShowInfor.php
@@ -57,7 +57,7 @@ class WarehouseShowInfor extends Component
       //Vars Blade Component
       $this->textClass = $textClass;
       $this->addressClass = $addressClass;
-      $this->user = \Auth::user() ?? null;
+      //$this->user = \Auth::user() ?? null;
       $this->$warehouseLocatorId = $warehouseLocatorId;
 
       //Shipping Method Base
@@ -175,6 +175,8 @@ class WarehouseShowInfor extends Component
   {
     \Log::info($this->log . 'setInitVars');
 
+    $this->user = \Auth::user() ?? null;
+    
     //Clean Values | Only Testing
     //$this->warehouseService()->cleanSessionVars();
 
