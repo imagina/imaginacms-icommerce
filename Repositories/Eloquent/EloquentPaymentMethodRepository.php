0c8c6b8cedd8eba739955ff70dd3ac39a71cfbfa
William <williamverde10@gmail.com>
2024-09-24 17:13:02 -0400

    Fix: variable name in Payment Method Repository

--- Repositories/Eloquent/EloquentPaymentMethodRepository.php
+++ Repositories/Eloquent/EloquentPaymentMethodRepository.php
@@ -39,25 +39,25 @@ class EloquentPaymentMethodRepository extends EloquentCrudRepository implements 
      * if (isset($filter->status)) $query->where('status', $filter->status);
      *
      */
-  
+
     $this->validateTenantWithCentralData($query);
-  
+
     if (isset($filter->search)) {
       //find search in columns
       $query->where('id', 'like', '%' . $filter->search . '%')
         ->orWhere('payment_code', 'like', '%' . $filter->search . '%')
         ->orWhere('name', 'like', '%' . $filter->search . '%');
     }
-    
-  
+
+
     if (isset($params->setting) && isset($params->setting->fromAdmin) && $params->setting->fromAdmin) {
-    
+
     } else {
       //pre-filter status
       $query->where("status", 1);
-    
+
     }
-    
+
     //Response
     return $query;
   }
@@ -86,17 +86,17 @@ class EloquentPaymentMethodRepository extends EloquentCrudRepository implements 
     //Response
     return $model;
   }
-  
+
   private function validateTenantWithCentralData($query)
   {
-    
+
     $entitiesWithCentralData = json_decode(setting("icommerce::tenantWithCentralData", null, "[]"));
     $tenantWithCentralData = in_array("paymentMethods", $entitiesWithCentralData);
-    
-    
+
+
     if ($tenantWithCentralData && isset(tenant()->id)) {
       $model = $this->model;
-      
+
       $query->withoutTenancy();
       $query->where(function ($query) use ($model) {
         $query->where($model->qualifyColumn(BelongsToTenant::$tenantIdColumn), tenant()->getTenantKey())
@@ -108,20 +108,20 @@ class EloquentPaymentMethodRepository extends EloquentCrudRepository implements 
       if( config("tenancy.mode")!=NULL && config("tenancy.mode")=="singleDatabase" && is_null(tenant()))
         $query->where("organization_id",null);
     }
-    
+
   }
-  
+
   public function getCalculations($params){
-  
+
     $items = [];
 
     if (isset($params->filter->withCalculations)) {
-      $filters = $params->filter;//Short data filter
+      $filter = $params->filter;//Short data filter
       $items = $this->getItemsBy($params);
-   
+
       if (isset($items) && $items->count() > 0) {
         $data = [];
-      
+
         if (isset($filter->cartId)) {
           // Add products to request
           $data['cartId'] = $filter->cartId;
@@ -129,40 +129,40 @@ class EloquentPaymentMethodRepository extends EloquentCrudRepository implements 
           if (isset($params->extra))
             $data['extra'] = $params->extra;
         }
-      
+
         //Helper to get current currency
         $currentCurrency = currentCurrency();
-      
-        foreach ($items as $key => $method) {
         
+        foreach ($items as $key => $method) {
+
           //Process to validate method currencies
           $methodDeleted = false;
           if(isset($filter->validateCurrency)){
-          
+
             /* If the field is not configured yet,
             the method will be displayed for all */
             if(isset($method->options->showInCurrencies)){
               $currencies = $method->options->showInCurrencies;
-            
+
               if(!in_array($currentCurrency->code, $currencies)){
                 unset($items[$key]);
                 $methodDeleted = true;
               }
             }
           }
-     
+
           if($methodDeleted==false){
             //Process to calculation validation in each method
             $methodApiController = app($method->options->init);
-          
+
             if (method_exists($methodApiController, "calculations")) {
               try {
-              
+
                 $results = $methodApiController->calculations(new Request ($data));
                 $resultData = $results->getData();
                 $method->calculations = $resultData;
               } catch (\Exception $e) {
-              
+
                 $resultData["msj"] = "error";
                 $resultData["items"] = $e->getMessage();
                 $method->calculations = $resultData;
@@ -172,7 +172,7 @@ class EloquentPaymentMethodRepository extends EloquentCrudRepository implements 
         }
       }
     }
-    
+
     return $items;
   }
 }
