3064ac5d915a6d33c04e1fe53c633cf268752858
Juan Escalante <jcec007@gmail.com>
2024-09-29 19:51:55 -0500

    +Update: removed add to cart dynamic button to avoid extra requests

--- Resources/views/frontend/components/product/buttons.blade.php
+++ Resources/views/frontend/components/product/buttons.blade.php
@@ -1,39 +1,22 @@
-<div
-  class="buttons {{$buttonsLayout}} {{$buttonsPosition}} {{$withTextInAddToCart ? "with-add-cart-text" : "without-add-cart-text"}}
-  {{$showButtonsOnMouseHover ? "show-on-mouse-hover" : ""}}">
+<div class="buttons {{$buttonsLayout}} {{$buttonsPosition}} {{$withTextInAddToCart ? "with-add-cart-text" : "without-add-cart-text"}}
+{{$showButtonsOnMouseHover ? "show-on-mouse-hover" : ""}}">
 @if(!$product->is_call && !$product->is_sold_out)
-  @switch($addToCartButtonAction ?? setting("icommerce::addToCartButtonAction"))
+  @switch(setting("icommerce::addToCartButtonAction"))
     @case("add-to-cart") @default
     @if(!$addToCartWithQuantity)
       <!-- add-cart btn-sm btn -->
-        @if(setting('icommerce::warehouseFunctionality', null, false))
-          <livewire:icommerce::addToCartButton
-            lazy
-            :wire:key="(uniqid('addToCartButton-'.$product->id))"
-            :onclick="'window.livewire.emit(\'addToCart\','.$product->id.',1,{},false)'"
-            :style=$buttonsLayout
-            buttonClasses="add-cart button-small"
-            :withIcon=$withIconInAddToCart
-            :iconClass="'fa '.$addToCartIcon"
-            :withLabel=$withTextInAddToCart
-            :label="$labelButtonAddProduct ?? trans('icommerce::products.button.addToCartItemList')"
-            :sizeLabel=$bottomFontSize
-            :product=$product
-          />
-        @else
-          <x-isite::button :style="$buttonsLayout" buttonClasses="add-cart button-small"
-                           :onclick="'window.livewire.emit(\'addToCart\','.$product->id.',1,{},false)'"
-                           :withIcon="$withIconInAddToCart"
-                           :iconClass="'fa '.$addToCartIcon"
-                           :withLabel="$withTextInAddToCart"
-                           :label="$labelButtonAddProduct ?? trans('icommerce::products.button.addToCartItemList')"
-                           :sizeLabel="$bottomFontSize"
-          />
-        @endif
+        <x-isite::button :style="$buttonsLayout" buttonClasses="add-cart button-small"
+                         :onclick="'window.livewire.emit(\'addToCart\','.$product->id.',1,{},false)'"
+                         :withIcon="$withIconInAddToCart"
+                         :iconClass="'fa '.$addToCartIcon"
+                         :withLabel="$withTextInAddToCart"
+                         :label="$labelButtonAddProduct ?? trans('icommerce::products.button.addToCartItemList')"
+                         :sizeLabel="$bottomFontSize"
+        />
+      
       @endif
       @break
       @case("go-to-show-view")
-
       <x-isite::button :style="$buttonsLayout" buttonClasses="add-cart button-small"
                        :href="$product->url"
                        :withIcon="$withIconInAddToCart"
@@ -42,13 +25,11 @@
                        :label="$labelButtonAddProduct ?? trans('icommerce::products.button.addToCartItemList')"
                        :sizeLabel="$bottomFontSize"
       />
-
       @break
     @endswitch
     @switch(setting("icommerce::addToCartQuoteButtonAction"))
       @case("add-to-cart-quote")
       @if(setting("icommerce::showButtonToQuoteInStore"))
-
         <x-isite::button :style="$buttonsLayout" buttonClasses="add-cart button-small"
                          :onclick="'window.livewire.emit(\'addToCart\','.$product->id.',1,{},false)'"
                          :withIcon="$withIconInAddToCart"
@@ -57,11 +38,10 @@
                          :sizeLabel="$bottomFontSize"
                          label="file"
         />
-
       @endif
     @endswitch
   @else
-
+    
     @php $contactUrl=setting('icommerce::customIndexContactLabel', null, 'Contáctenos'); @endphp
     <x-isite::button :style="$buttonsLayout" buttonClasses="contact button-small"
                      :withIcon="$withIconInAddToCart"
@@ -70,12 +50,10 @@
                      :withLabel="$withTextInAddToCart"
                      :label="$contactUrl"
                      :sizeLabel="$bottomFontSize"
-
     />
-
   @endif
   @if((($withTextInAddToCart && $addToCartWithQuantity) || !$addToCartWithQuantity) && $wishlistEnable)
-
+    
     @php $wishUrl=json_encode(["entityName" => "Modules\\Icommerce\\Entities\\Product", "entityId" => $product->id]); @endphp
     <x-isite::button :style="$buttonsLayout" buttonClasses="wishlist button-small"
                      :onclick="'window.livewire.emit(\'addToWishList\','.$wishUrl.')'"
@@ -84,9 +62,7 @@
                      :withLabel="false"
                      :sizeLabel="$bottomFontSize"
                      :label="trans('icommerce::products.button.wishList')"
-
+    
     />
-
   @endif
 </div>
-      
