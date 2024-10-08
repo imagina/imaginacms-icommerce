205b8d212205d8ac843e1f0f6a3899ab6a278f3b
William <williamverde10@gmail.com>
2024-10-08 12:02:16 -0400

    Added: days before, days suspension | Subscriptions Migration | Recurrence

---
+++ Database/Migrations/2024_10_08_185350005135_add_days_in_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDaysInSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('icommerce__subscriptions', function (Blueprint $table) {
+        
+        $table->integer('days_before_due')->unsigned()->default(0)->after('due_date');
+        $table->integer('days_for_suspension')->unsigned()->default(0)->after('days_before_due');
+       
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::table('icommerce__subscriptions', function (Blueprint $table) {
+
+        $table->dropColumn('days_before_due');
+        $table->dropColumn('days_for_suspension');
+        
+      });
+    }
+}
