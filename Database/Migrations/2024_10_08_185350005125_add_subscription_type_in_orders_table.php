65d0f9d30f90a8fbeb7df7ac98b7c083c201f8dd
William <williamverde10@gmail.com>
2024-10-08 11:58:32 -0400

    Added:subscription type in Orders | Recurrence

---
+++ Database/Migrations/2024_10_08_185350005125_add_subscription_type_in_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('icommerce__orders', function (Blueprint $table) {
+    
+            $table->string('subscription_type')->nullable()->after('subscription_token');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('icommerce__orders', function (Blueprint $table) {
+
+            $table->dropColumn('subscription_type');
+            
+        });
+    }
+
+};
