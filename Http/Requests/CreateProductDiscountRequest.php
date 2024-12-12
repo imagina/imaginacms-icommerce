732ca18ece65079ddd7e4246ec9368c3574eb84a
William <williamverde10@gmail.com>
2024-12-12 18:01:51 -0400

    Added: validation to Requests , product, product warehouse, product discount

--- Http/Requests/CreateProductDiscountRequest.php
+++ Http/Requests/CreateProductDiscountRequest.php
@@ -8,7 +8,11 @@ class CreateProductDiscountRequest extends BaseFormRequest
 {
     public function rules()
     {
-        return [];
+        return [
+            'discount' => 'required',
+            'date_start' => 'required|date_format:Y-m-d',
+            'date_end' => 'required|date_format:Y-m-d'
+        ];
     }
 
     public function translationRules()
