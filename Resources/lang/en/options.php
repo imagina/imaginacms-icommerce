8624ae1bd5627db65a48210698389dde52068650
Cristian Marin <marin101998@gmail.com>
2025-01-22 15:55:53 -0500

    + fix | v10 22-01-25 add translation

--- Resources/lang/en/options.php
+++ Resources/lang/en/options.php
@@ -1,33 +1,34 @@
 <?php
 
 return [
-    'list resource' => 'List options',
-    'create resource' => 'Create options',
-    'edit resource' => 'Edit options',
-    'destroy resource' => 'Destroy options',
-    'title' => [
-        'options' => 'Option',
-        'create option' => 'Create a option',
-        'edit option' => 'Edit a option',
-    ],
-    'button' => [
-        'create option' => 'Create a option',
-    ],
-    'table' => [
-    ],
-    'form' => [
-    ],
-    'messages' => [
-    ],
-    'validation' => [
-    ],
-    'types' => [
-        'checkbox' => 'Multiple selection (list)',
-        'radio' => 'Single selection (list)',
-        'select' => 'Single selection (Selector)',
-        'text' => 'Short text',
-        'textarea' => 'Long text',
-        'date' => 'Date',
-        'file' => 'File',
-    ],
+  'list resource' => 'List options',
+  'create resource' => 'Create options',
+  'edit resource' => 'Edit options',
+  'destroy resource' => 'Destroy options',
+  'title' => [
+    'options' => 'Option',
+    'create option' => 'Create a option',
+    'edit option' => 'Edit a option',
+  ],
+  'button' => [
+    'create option' => 'Create a option',
+  ],
+  'table' => [
+  ],
+  'form' => [
+  ],
+  'messages' => [
+  ],
+  'validation' => [
+  ],
+  'types' => [
+    'checkbox' => 'Multiple selection (list)',
+    'radio' => 'Single selection (list)',
+    'select' => 'Single selection (Selector)',
+    'text' => 'Short text',
+    'textarea' => 'Long text',
+    'date' => 'Date',
+    'file' => 'File',
+    'color_image' => 'Single Selection (Color)'
+  ],
 ];
