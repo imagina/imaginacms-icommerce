1ab162e0d23647ab8c356f6a4c15016ee4dff99e
Cristian Marin <marin101998@gmail.com>
2025-01-23 15:37:28 -0500

    + fix | v10 23-01-25 validate dynamic gallery only in option type color-image

--- Http/Livewire/Options/ItemOption.php
+++ Http/Livewire/Options/ItemOption.php
@@ -93,10 +93,12 @@ class ItemOption extends Component
 
     $this->emit('updateOption', $oldValue, $this->selected, $this->dynamic, $this->optionId);
 
-    if (!is_null($this->selected)) {
-      $this->emit('updateGallery', json_decode(json_encode($productOptionValueMediaFiles)));
-    } else {
-      $this->emit('updateGallery', $this->product->mediaFiles());
+    if ($this->type == "color_image") {
+      if (!is_null($this->selected)) {
+        $this->emit('updateGallery', json_decode(json_encode($productOptionValueMediaFiles)));
+      } else {
+        $this->emit('updateGallery', $this->product->mediaFiles());
+      }
     }
   }
 
