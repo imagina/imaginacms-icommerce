cebdede8a709606bd79c5b1477f8d89f64ef2581
Cristian Marin <marin101998@gmail.com>
2025-02-25 15:56:25 -0500

    + fix | v10 25-02-25 change info component cart with options descriptions

--- Resources/views/frontend/livewire/cart/productOptions.blade.php
+++ Resources/views/frontend/livewire/cart/productOptions.blade.php
@@ -1,15 +1,21 @@
 @if($cartProduct->productOptionValues->count())
-    <br>
-    @foreach($cartProduct->productOptionValues as $productOptionValue)
-        <label>{{$productOptionValue->option->description}}
-            : {{$productOptionValue->optionValue->description}}</label>
-    @endforeach
+  <br>
+  @foreach($cartProduct->productOptionValues as $productOptionValue)
+    <label>{{$productOptionValue->option->description}}
+      : {{$productOptionValue->optionValue->description}}</label>
+  @endforeach
 @endif
 
 @if($cartProduct->dynamicOptions)
-    <br>
-    @foreach($cartProduct->dynamicOptions as $option)
-        <label>{{$option->description}}
-            : {{$option->pivot->value}}</label>
-    @endforeach
+  <br>
+  @foreach($cartProduct->dynamicOptions as $option)
+    @php
+      $productOptionValueRepository = app('Modules\Icommerce\Repositories\ProductOptionValueRepository');
+      $optionValueRepository = app('Modules\Icommerce\Repositories\OptionValueRepository');
+      $productOptionValue = $productOptionValueRepository->getItem($option->pivot->value);
+      $optionValue = $optionValueRepository->getItem($productOptionValue->option_value_id);
+    @endphp
+    <label>{{$option->description}}
+      : {{$optionValue->description ?? $option->pivot->value}}</label>
+  @endforeach
 @endif
