94e02a7f8e06a0acc6205cb74dc01a7786f9c2ab
Juan Escalante <jcec007@gmail.com>
2025-03-31 13:08:56 -0500

    +Update: validate if the details field exist

--- Resources/views/frontend/components/product/addToCartWithQuantity.blade.php
+++ Resources/views/frontend/components/product/addToCartWithQuantity.blade.php
@@ -89,7 +89,7 @@
         console.warn(addBtn);
         let divParentAdd = addBtn.closest('div.add-to-cart-with-quantity')
         let divParentDetails = addBtn.closest('div.col.no-padding').siblings('div.product-details');
-        let detailsTextarea = $(divParentDetails).children("textarea")[0].value;
+        let detailsTextarea = $(divParentDetails).children("textarea")[0]?.value ?? '';
         let quantitySelector = $(divParentAdd).children("div.number-input.input-group.quantity-selector")[0];
         let quantityInput = $(quantitySelector).children("input.quantity-field")[0];
         let productId = $(e.target).data('item-id');
