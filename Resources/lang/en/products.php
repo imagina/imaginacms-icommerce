aebfa41e6484a533c56d37e42a91ae6d64c7df6d
adrianacardoso <acardosof27@gmail.com>
2025-04-03 14:01:58 -0400

    +update: optimized WhatsApp button

--- Resources/lang/en/products.php
+++ Resources/lang/en/products.php
@@ -22,6 +22,7 @@ return [
         'create product' => 'Create a product',
         'addToCartItemList' => 'Buy',
         'wishList' => 'Wish List',
+        'buyShowWhatsApp' => 'Buy by WhatsApp',
         'labelBuyWhatsApp' => 'Buy'
     ],
     'table' => [
--- Resources/views/frontend/livewire/options/index.blade.php
+++ Resources/views/frontend/livewire/options/index.blade.php
@@ -115,23 +115,7 @@
               </div>
             @endif
 
-            @php
-            $numberBuyWhatsApp = json_decode(setting(setting('icommerce::productSelectSettingButtonBuyWhatsApp', 'isite::whatsapp1')))
-            @endphp
-            @if(!empty(trim($numberBuyWhatsApp->number)) && setting('icommerce::productShowButtonBuyWhatsApp',null,false))
-              <div class="d-inline-flex align-items-center p-1">
-                <!-- BUTTON WHATSAPP -->
-                @php
-                    $textBuyWhatsApp = setting('icommerce::productShowButtonBuyWhatsAppTextMessage').' *'. $product->name. '* ' . url()->full()
-                @endphp
-                <a class="btn buy-whatsapp" aria-label="buy whatsapp"
-                   href="https://wa.me/{{$numberBuyWhatsApp->callingCode . $numberBuyWhatsApp->number}}?text={{ urlencode($textBuyWhatsApp) }}"
-                   target="_blank">
-                  <i class="fa fa-whatsapp"></i>
-                  <span>{{ trans('icommerce::products.button.buyWhatsApp') }}</span>
-                </a>
-              </div>
-            @endif
+            @include('icommerce::frontend.partials.show.buttonWhatsApp')
 
           </div>
 
@@ -145,13 +129,6 @@
 @section('scripts-owl')
   @parent
   <style>
-    #content_show_commerce .information .buy-whatsapp {
-      background-color: var(--color-by-whatsapp, #25D366);
-      color: #ffffff;
-    &:hover {
-       background-color: color-mix(in srgb, var(--color-by-whatsapp, #25D366) 95%, black);
-     }
-    }
     #content_show_commerce .information .button-primary {
       background-color: var(--primary);
     }
