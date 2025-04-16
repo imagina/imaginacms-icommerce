2e721ed25f8ed94d9edcd666f56bdb1c6be7b645
Juan Escalante <jcec007@gmail.com>
2025-04-16 17:33:15 -0500

    +Fix: Several events passed to entities were causing errors resulting from the model now being sent differently within a dataset according to the EloquentCrudRepository

--- Events/Handlers/ValidateQuantitiesWarehouse.php
+++ Events/Handlers/ValidateQuantitiesWarehouse.php
@@ -21,7 +21,7 @@ class ValidateQuantitiesWarehouse
 
   public function handle($event)
   {
-    $productOptionValueWarehouse = $event->entity;
+    $productOptionValueWarehouse = $event->entity["model"];
 
     //se busca en la table product option value con el id de la opcion que ese esta actualizando
     $productOptionValue = \DB::table('icommerce__product_option_value')
