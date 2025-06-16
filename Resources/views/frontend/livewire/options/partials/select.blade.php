0294018498b3dcb90ee9d8d2d3d2819785a821d4
Julian <65859890+TJULIAN-D@users.noreply.github.com>
2025-06-16 12:40:38 -0500

    + Fix : Front-End | Improve select options display and formatting (#271)

--- Resources/views/frontend/livewire/options/partials/select.blade.php
+++ Resources/views/frontend/livewire/options/partials/select.blade.php
@@ -1,10 +1,15 @@
 @if(!empty($productOptionValues))
-  <select class="form-control" {{$productOption->required ? "required" : ""}} wire:model="selected">
-    <option value="NULL">selecciona una opción</option>
+  <select class="form-control" {{ $productOption->required ? "required" : "" }} wire:model="selected">
+    <option value="Null">Selecciona una opción</option>
     @foreach($productOptionValues as $selectOption)
-      <option {{!$selectOption->available ? "disabled" : ""}} value="{{$selectOption->id}}">
-        {{ $selectOption->optionValue->description }} {{$selectOption->price != 0 ? (formatMoney($selectOption->price)) : ''}}
-        {{ !$selectOption->available ? '(Agotado)' : ''}}
+      <option
+        {{ !$selectOption->available ? 'disabled' : '' }}
+        value="{{ $selectOption->id }}"
+      >
+        {{ $selectOption->optionValue->description }}
+        {!! $selectOption->quantity == 0 ? '' : '- ('.$selectOption->quantity.') ' !!}
+        {{ $selectOption->price != 0 ? ' - ' . formatMoney($selectOption->price) : '' }}
+        {{ !$selectOption->available ? '(Agotado)' : '' }}
       </option>
     @endforeach
   </select>
