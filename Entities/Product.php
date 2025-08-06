424c268438d4dd906170598441a624991d9c10cf
Cristian Marin <marin101998@gmail.com>
2025-08-06 11:37:32 -0500

    + fix | v10 06-08-25 init parameter currentCategoryUrls

--- Entities/Product.php
+++ Entities/Product.php
@@ -614,6 +614,7 @@ class Product extends CrudModel implements TaggableInterface
       $baseUrls[] = $this->organization->url;
     }
 
+    $currentCategoryUrls = [];
     foreach ($this->categories as $category) {
       $currentCategoryUrls = array_merge($currentCategoryUrls, $category->getAllLocalizedUrls());
     }
